386f7bd1ca6da727d89cc8bcf448967895a2466c
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-05 00:06:29 +0530

    Time: 23 ms (8.24%), Space: 8.5 MB (98.43%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,37 +2,41 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int row=triangle.size();
-        int col=-1;
-        for(int i=0;i<row;i++)
-        {
-            int curr=triangle[i].size();
-            col=max(col,curr);
-        }
-        vector<vector<int>> grid(row,vector<int>(col,1e9));
-        for(int i=0;i<row;i++)
+        for(int i =0;i<row;i++)
         {
             int curr=triangle[i].size();
             for(int j=0;j<curr;j++)
             {
-                grid[i][j]=triangle[i][j];
+                if(i==0 && j==0) continue;
+                if(i==j)
+                {
+                    triangle[i][j]+=triangle[i-1][j-1];
+                }
+                else if(j==0 && i!=0)
+                {
+                    triangle[i][j]+=triangle[i-1][j];
+                }
+                else
+                {
+                    triangle[i][j]+=min(triangle[i-1][j],triangle[i-1][j-1]);
+                }
             }
         }
-        vector<vector<int>> dp(row+1,vector<int>(col+1,1e9));
-        dp[1][1]=grid[0][0];
-        for(int i =1;i<=row;i++)
+        for(int i =0;i<row;i++)
         {
-            int curr=triangle[i-1].size();
-            for(int j=1;j<=curr;j++)
+            int curr=triangle[i].size();
+            for(int j=0;j<curr;j++)
             {
-                if(i==1 && j==1) continue;
-                dp[i][j]=grid[i-1][j-1]+min(dp[i-1][j],dp[i-1][j-1]);
+                cout<<triangle[i][j]<<" ";
             }
+            cout<<endl;
         }
         int ans=1e9;
-        for(int i=1;i<=col;i++)
+        for(int i=0;i<triangle[row-1].size();i++)
         {
-            ans=min(ans,dp[row][i]);
+            ans=min(ans,triangle[row-1][i]);
         }
         return ans;
+        
     }
 };
