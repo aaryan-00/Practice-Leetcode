bd37bc1d7d66f785ecfe9d0ce4aa14d08a9ce104
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-05 23:23:47 +0530

    Time: 21 ms (58.32%), Space: 11.9 MB (49.36%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -28,17 +28,37 @@ public:
         }
         return dp[ind][target]=tkn+ntkn;
     }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
+    int findTargetSumWays(vector<int>& num, int target) {
+        int n=num.size();
         int sum=0;
         for(int i=0;i<n;i++)
         {
-            sum+=nums[i];
+            sum+=num[i];
         }
-        int diff=sum-target;
-        if(diff<0 || diff&1) return 0;
-        diff=diff/2;
-        vector<vector<int>> dp(n,vector<int>(diff+1,-1));
-        return recur(n-1,nums,diff,dp);
+        int tar=sum-target;
+        if(tar<0 || tar&1) return 0;
+        tar/=2;
+        // vector<vector<int>> dp(n,vector<int>(tar+1,-1));
+        // return recur(n-1,num,tar,dp);
+        vector<vector<int>> dp(n,vector<int>(tar+1,0));
+    
+        if(num[0] == 0) dp[0][0] =2;  // 2 cases -pick and not pick
+        else dp[0][0] = 1;  // 1 case - not pick
+        
+        if(num[0]!=0 && num[0]<=tar) dp[0][num[0]] = 1;  // 1 case -pick
+        
+        for(int ind = 1; ind<n; ind++){
+            for(int target= 0; target<=tar; target++){
+                
+                int notTaken = dp[ind-1][target];
+        
+                int taken = 0;
+                    if(num[ind]<=target)
+                        taken = dp[ind-1][target-num[ind]];
+            
+                dp[ind][target]= (notTaken + taken);
+            }
+        }
+        return dp[n-1][tar];
     }
 };
