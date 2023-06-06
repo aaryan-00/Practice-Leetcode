d1408b875c895cf5b25fecd210b3c902534d3f19
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-06 17:38:46 +0530

    Time: 1295 ms (12.71%), Space: 449.9 MB (6.13%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -17,10 +17,40 @@ public:
         }
     }
     string longestPalindrome(string s) {
-        for(int i=0;i<s.size();i++)
+        // for(int i=0;i<s.size();i++)
+        // {
+        //     find_palindrome(s,i,i);
+        //     find_palindrome(s,i,i+1);
+        // }
+        // return ans;
+        string t=s;
+        reverse(t.begin(),t.end());
+        int n=s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        int maxi=0;
+        string ans="";
+        for(int i=1;i<=n;i++)
         {
-            find_palindrome(s,i,i);
-            find_palindrome(s,i,i+1);
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                if(dp[i][j]>maxi)
+                {
+                    int s_Ind=i-dp[i][j];
+                    string curr=s.substr(s_Ind,dp[i][j]);
+                    string temp=curr;
+                    reverse(curr.begin(),curr.end());
+                    if(curr==temp)
+                    {
+                        maxi=dp[i][j];
+                        ans=curr;
+
+                    }
+                }
+            }
         }
         return ans;
     }
