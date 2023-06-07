4dbd8a24237477ebf0b0c54a0f5797c73a6f17e7
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-07 19:12:04 +0530

    Time: 45 ms (70.23%), Space: 18.6 MB (55.07%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int Memo(string &s,string &t,int i,int j,vector<vector<int>> &dp)
+    {
+        if(j<0)
+        {
+            return 1;
+        }
+        if(i<0)
+        {
+            return 0;
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int ans=0;
+        if(s[i]==t[j])
+        {
+            // return dp[i][j]=Memo(s,t,i-1,j-1,dp)+Memo(s,t,i-1,j,dp); 
+            ans+=Memo(s,t,i-1,j-1,dp);
+            ans+=Memo(s,t,i-1,j,dp);   // return dp[i][j]+=Memo(s,t,i-1,j-1,dp);
+        }
+        else
+        {
+            ans+=Memo(s,t,i-1,j,dp);
+            // return dp[i][j]= Memo(s,t,i-1,j,dp);
+        }
+        return dp[i][j]=ans;
+    }
+    int numDistinct(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return Memo(s,t,n-1,m-1,dp);
+    }
+};
