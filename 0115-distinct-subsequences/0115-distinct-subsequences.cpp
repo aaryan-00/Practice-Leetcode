c7d97e2ca50f9a878d6ccfd255e9af738e0aba68
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-07 19:27:16 +0530

    Time: 47 ms (66.91%), Space: 18.5 MB (67.51%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -24,7 +24,7 @@ public:
         // }
 
         //orrr
-        
+
         int ans=0;
         if(s[i]==t[j])
         {
@@ -40,7 +40,33 @@ public:
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return Memo(s,t,n-1,m-1,dp);
+        // vector<vector<int>> dp(n,vector<int>(m,-1));
+        // return Memo(s,t,n-1,m-1,dp);
+
+        //tabulation
+
+        vector<vector<unsigned>> dp(n+1,vector<unsigned>(m+1,0));
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=1;
+        }
+        for(int i=1;i<m+1;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
