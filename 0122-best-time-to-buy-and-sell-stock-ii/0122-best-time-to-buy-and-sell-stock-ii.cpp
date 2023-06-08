cd68e17a2fd0b72d54c3bf65c78b64f360cd883f
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-08 16:52:04 +0530

    Time: 21 ms (5.42%), Space: 17.2 MB (10.35%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -10,22 +10,27 @@ public:
         {
             return dp[ind][buy];
         }
+        int profit=0;
         if(buy==1)
         {
-            //bought
-            int buyy=-p[ind]+memo(p,ind+1,0,dp);
-            //not bought
-            int nbuyy=0+memo(p,ind+1,1,dp);
-            return dp[ind][buy]= max(buyy,nbuyy);
+            // //bought
+            // int buyy=-p[ind]+memo(p,ind+1,0,dp);
+            // //not bought
+            // int nbuyy=0+memo(p,ind+1,1,dp);
+            // return dp[ind][buy]= max(buyy,nbuyy);
+            profit=max(-p[ind]+memo(p,ind+1,0,dp),memo(p,ind+1,1,dp));
+
         }
         else
         {
-            //sold
-            int sold=p[ind]+memo(p,ind+1,1,dp);
-            //not sold
-            int nsold=0+memo(p,ind+1,0,dp);
-            return dp[ind][buy]= max(sold,nsold);
+            // //sold
+            // int sold=p[ind]+memo(p,ind+1,1,dp);
+            // //not sold
+            // int nsold=0+memo(p,ind+1,0,dp);
+            // return dp[ind][buy]= max(sold,nsold);
+            profit=max(p[ind]+memo(p,ind+1,1,dp),memo(p,ind+1,0,dp));
         }
+        return dp[ind][buy]=profit;
 
     }
     int maxProfit(vector<int>& prices) {
