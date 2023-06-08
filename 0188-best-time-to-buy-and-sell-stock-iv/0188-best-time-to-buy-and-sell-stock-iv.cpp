e77ab6b8ff9d9c0bc82b1d3996d00455c103640a
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-08 19:29:16 +0530

    Time: 32 ms (32.58%), Space: 14.5 MB (31.85%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -10,42 +10,40 @@ public:
         {
             return dp[ind][buy][transactions];
         }
-        int profit=0;
         if(buy==1)
         {
-            profit=max(-p[ind]+memo(p,ind+1,0,dp,transactions),memo(p,ind+1,1,dp,transactions));
+            return dp[ind][buy][transactions]=max(-p[ind]+memo(p,ind+1,0,dp,transactions),memo(p,ind+1,1,dp,transactions));
         }
         else
         {
-            profit=max(p[ind]+memo(p,ind+1,1,dp,transactions-1),memo(p,ind+1,0,dp,transactions));
+            return dp[ind][buy][transactions]=max(p[ind]+memo(p,ind+1,1,dp,transactions-1),memo(p,ind+1,0,dp,transactions));
         }
-        return dp[ind][buy][transactions]=profit;
 
     }
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        // vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
-        // return memo(prices,0,1,dp,k);
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        return memo(prices,0,1,dp,k);
 
         // space optimisation(tabulation)
-        vector<vector<int>> next(2,vector<int>(k+1,0));
-        for(int ind=n-1;ind>=0;ind--)
-        {
-            for(int buy=0;buy<=1;buy++)
-            {
-                for(int cap=1;cap<=k;cap++)
-                {
-                    if(buy==1)
-                    {
-                        next[buy][cap]=max(-prices[ind]+next[0][cap],next[1][cap]);
-                    }
-                    else
-                    {
-                        next[buy][cap]=max(prices[ind]+next[1][cap-1],next[0][cap]);
-                    }
-                }
-            }
-        }
-        return next[1][k];
+        // vector<vector<int>> next(2,vector<int>(k+1,0));
+        // for(int ind=n-1;ind>=0;ind--)
+        // {
+        //     for(int buy=0;buy<=1;buy++)
+        //     {
+        //         for(int cap=1;cap<=k;cap++)
+        //         {
+        //             if(buy==1)
+        //             {
+        //                 next[buy][cap]=max(-prices[ind]+next[0][cap],next[1][cap]);
+        //             }
+        //             else
+        //             {
+        //                 next[buy][cap]=max(prices[ind]+next[1][cap-1],next[0][cap]);
+        //             }
+        //         }
+        //     }
+        // }
+        // return next[1][k];
     }
 };
