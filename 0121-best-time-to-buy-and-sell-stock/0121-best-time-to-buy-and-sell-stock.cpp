6a2b76db35719c01662777524c67e024e8b92943
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-08 16:23:45 +0530

    Time: 144 ms (59.03%), Space: 93.2 MB (78.67%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,41 +1,46 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // int n=prices.size();
-        // int max_profit=0;
-        // int min_buy=prices[0];
-        // for(int i=1;i<n;i++)
-        // {
-        //     if(min_buy<prices[i])
-        //     {
-        //         max_profit=max(max_profit,prices[i]-min_buy);
-        //     }
-        //     // if(min_buy>prices[i])
-        //     // {
-        //     //     min_buy=prices[i];
-        //     // }
-        //     min_buy=min(min_buy,prices[i]);
-        // }
-        // return max_profit;
+        int n=prices.size();
+        int max_profit=0;
+        int min_buy=prices[0];
+        for(int i=1;i<n;i++)
+        {
+            int cost=prices[i]-min_buy;
+            max_profit=max(max_profit,cost);
+            //or
+            // if(min_buy<prices[i])
+            // {
+            //     max_profit=max(max_profit,prices[i]-min_buy);
+            // }
 
-        int len=prices.size();
-        if(len==0) return 0;
-        int profit=0;
-        vector<int> minn(len-1);
-        int m=INT_MAX;
-        for(int i=0;i<len-1;i++)
-        {
-            if(prices[i]<m) m=prices[i];
-            minn[i]=m;
+            // if(min_buy>prices[i])
+            // {
+            //     min_buy=prices[i];
+            // }
+            //or
+            min_buy=min(min_buy,prices[i]);
         }
-        for(int i=1;i<len;i++)
-        {
-            if(prices[i]-minn[i-1]>profit)
-            {
-                profit=prices[i]-minn[i-1];
-            }
-        }
-        return profit;
+        return max_profit;
+
+        // int len=prices.size();
+        // if(len==0) return 0;
+        // int profit=0;
+        // vector<int> minn(len-1);
+        // int m=INT_MAX;
+        // for(int i=0;i<len-1;i++)
+        // {
+        //     if(prices[i]<m) m=prices[i];
+        //     minn[i]=m;
+        // }
+        // for(int i=1;i<len;i++)
+        // {
+        //     if(prices[i]-minn[i-1]>profit)
+        //     {
+        //         profit=prices[i]-minn[i-1];
+        //     }
+        // }
+        // return profit;
 
     }
 };
