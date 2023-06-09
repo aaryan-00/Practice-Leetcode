2e81c5a17f898d0605cac892dddaa5a36c2a3bff
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-09 23:32:28 +0530

    Time: 91 ms (89.7%), Space: 17.3 MB (53.16%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,55 +1,81 @@
 class Solution {
 public:
-    bool check(string &a,string &b)
-    {
-        if(a.size()!=b.size()+1)
-        {
-            return false;
-        }
-        int i=0,j=0;
-        while(i<a.size())
-        {
-            if(j<b.size() && a[i]==b[j])
-            {
-                i++;
-                j++;
-            }
-            else
-            {
-                i++;
-            }
-        }
-        if(i==a.size() && j==b.size())
-        {
-            return true;
-        }
-        return false;
-    }
+    // bool check(string &a,string &b)
+    // {
+    //     if(a.size()!=b.size()+1)
+    //     {
+    //         return false;
+    //     }
+    //     int i=0,j=0;
+    //     while(i<a.size())
+    //     {
+    //         if(j<b.size() && a[i]==b[j])
+    //         {
+    //             i++;
+    //             j++;
+    //         }
+    //         else
+    //         {
+    //             i++;
+    //         }
+    //     }
+    //     if(i==a.size() && j==b.size())
+    //     {
+    //         return true;
+    //     }
+    //     return false;
+    // }
     static bool comp(string& s1, string& s2)
     {
         return s1.size() < s2.size();
     }
+    // int longestStrChain(vector<string>& words) {
+    //     int n=words.size();
+    //     sort(words.begin(), words.end(),comp);
+    //     vector<int> dp(n,1);
+    //     for(int i=1; i<=n-1; i++)
+    //     {
+    //         for(int prev_index = 0; prev_index <=i-1; prev_index ++)
+    //         {
+    //             if(check(words[i],words[prev_index]) && 1+dp[prev_index]>dp[i] )
+    //             {
+    //                 dp[i] =1 + dp[prev_index];
+    //             }
+    //         }
+    //     }
+    //     int ans = -1;
+    //     for(int i=0; i<=n-1; i++){
+    //         if(dp[i]>ans)
+    //         {
+    //             ans=dp[i];
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+    //map
     int longestStrChain(vector<string>& words) {
         int n=words.size();
         sort(words.begin(), words.end(),comp);
-        vector<int> dp(n,1);
-        for(int i=1; i<=n-1; i++)
+        unordered_map<string,int> mp;
+        int maxi=0;
+        for(auto curr:words)
         {
-            for(int prev_index = 0; prev_index <=i-1; prev_index ++)
+            for(int i=0;i<curr.size();i++)
             {
-                if(check(words[i],words[prev_index]) && 1+dp[prev_index]>dp[i] )
+                string temp=curr.substr(0,i)+curr.substr(i+1);
+                if(mp.find(temp)!=mp.end())
                 {
-                    dp[i] =1 + dp[prev_index];
+                    mp[curr]=max(mp[curr],1+mp[temp]);
+                }
+                else
+                {
+                    mp[curr]=max(mp[curr],1);
                 }
             }
+            maxi=max(maxi,mp[curr]);
+
         }
-        int ans = -1;
-        for(int i=0; i<=n-1; i++){
-            if(dp[i]>ans)
-            {
-                ans=dp[i];
-            }
-        }
-        return ans;
+        return maxi;
     }
 };
