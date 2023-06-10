fdd1570cead637c2b9021896fed9442611947a62
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-10 15:52:19 +0530

    Time: 441 ms (55.56%), Space: 6.7 MB (50.66%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -33,9 +33,36 @@ public:
         }
         return dp[i]=cost;
     }
+
+    int memo2(int i,int j,string &s,vector<int> &dp)
+    {
+        if(i==j)
+        {
+            return 0;
+        }
+        if(checkPalin(s,i,j))
+        {
+            return 0;
+        }
+        if(dp[i]!=-1)
+        {
+            return dp[i];
+        }
+        int cost=INT_MAX;
+        for(int k=i;k<j;k++)
+        {
+            if(checkPalin(s,i,k))
+            {
+                int curr=memo2(k+1,j,s,dp)+1;
+                cost=min(cost,curr);
+            }  
+        }
+        return dp[i]=cost;
+    }
     int minCut(string s) {
         int n=s.size();
         vector<int> dp(n,-1);
-        return memo(0,n,s,dp)-1;
+        // return memo(0,n,s,dp)-1;
+        return memo2(0,n-1,s,dp);
     }
 };
