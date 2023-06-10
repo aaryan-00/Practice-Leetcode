84c75c224179f3aedf47568710ab6909838e4922
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-10 15:46:27 +0530

    Time: 502 ms (40.31%), Space: 6.6 MB (58.7%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool checkPalin(string &s,int i,int j)
+    {
+        while(i<j)
+        {
+            if(s[i]!=s[j])
+            {
+                return false;
+            }
+            i++;j--;
+        }
+        return true;
+    }
+    int memo(int i,int j,string &s,vector<int> &dp)
+    {
+        if(i==j)
+        {
+            return 0;
+        }
+        if(dp[i]!=-1)
+        {
+            return dp[i];
+        }
+        int cost=INT_MAX;
+        for(int k=i;k<j;k++)
+        {
+            if(checkPalin(s,i,k))
+            {
+                int curr=memo(k+1,j,s,dp)+1;
+                cost=min(cost,curr);
+            }   
+        }
+        return dp[i]=cost;
+    }
+    int minCut(string s) {
+        int n=s.size();
+        vector<int> dp(n,-1);
+        return memo(0,n,s,dp)-1;
+    }
+};
