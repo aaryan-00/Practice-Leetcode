d3e42ccc7b38a2a5c070462091e5e7c5241c3b52
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-11 18:45:40 +0530

    Time: 63 ms (71.48%), Space: 10.3 MB (19.77%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -22,8 +22,28 @@ public:
         int c=cuts.size();
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
-        vector<vector<int>> dp(c+1,vector<int>(c+1,-1));
+        // vector<vector<int>> dp(c+1,vector<int>(c+1,-1));
         sort(cuts.begin(),cuts.end());
-        return recur(1,c,cuts,dp);
+        // return recur(1,c,cuts,dp);
+
+        //tabulation
+        //i:1->c+1 && j:c->0
+        vector<vector<int>> dp(c+2,vector<int>(c+2,0));
+        for(int i=c+1;i>=1;i--)
+        {
+            for(int j=0;j<=c;j++)
+            {
+                if(i>j) continue;
+                int mini=INT_MAX;
+                for(int k=i;k<=j;k++)
+                {
+                    int cost=cuts[j+1]-cuts[i-1]+dp[i][k-1]+dp[k+1][j];
+                    mini=min(mini,cost);
+                }
+                dp[i][j]=mini;
+                
+            }
+        }
+        return dp[1][c];
     }
 };
