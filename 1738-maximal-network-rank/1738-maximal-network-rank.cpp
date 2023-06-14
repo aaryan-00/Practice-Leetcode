ca6326a0c16e42d05736b72c679cf90c508d9de3
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-15 00:38:11 +0530

    Time: 138 ms (28.16%), Space: 36.5 MB (44.19%) - LeetHub

--- 1738-maximal-network-rank/1738-maximal-network-rank.cpp
+++ 1738-maximal-network-rank/1738-maximal-network-rank.cpp
@@ -9,20 +9,13 @@ public:
             indegree[i[1]]++;
             s.insert({i[0],i[1]});
         }
-        vector<pair<int,int>> pa;
-        for(int i=0;i<n;i++)
-        {
-            pa.push_back({indegree[i],i});
-        }
-        sort(pa.begin(),pa.end());
         int ans=-1;
         for(int i=n-1;i>0;i--)
         {
             for(int j=i-1;j>=0;j--)
             {
-                int curr=pa[j].first+pa[i].first;
-                int u=pa[j].second,v=pa[i].second;
-                if(s.count({u,v})||s.count({v,u}))
+                int curr=indegree[i]+indegree[j];
+                if(s.count({i,j})||s.count({j,i}))
                 {
                     curr--;
                 }
