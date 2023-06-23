1f567cb62dcfd06ecedef8a731ce8a629f7708f9
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-23 19:38:35 +0530

    Time: 9 ms (23.62%), Space: 8.9 MB (55.59%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -6,20 +6,25 @@ public:
         while(s<e)
         {
             int mid=s+e >>1;
-            if(mid==0)
-            { 
-                if(nums[mid]>=nums[mid+1]) return mid;
-                else return mid+1;
-            }
-            if(mid==n-1 )
+            // if(mid==0)
+            // { 
+            //     if(nums[mid]>=nums[mid+1]) return mid;
+            //     else return mid+1;
+            // }
+            // if(mid==n-1 )
+            // {
+            //     if(nums[mid]>=nums[mid-1]) return mid;
+            //     else return mid-1;
+            // }
+            // if(nums[mid]>=nums[mid-1] && nums[mid]>=nums[mid+1]) return mid;
+            // else if(nums[mid]<nums[mid-1])
+            // {
+            //     e=mid-1;
+            // }
+            // else s=mid+1;
+            if(nums[mid]>nums[mid+1])
             {
-                if(nums[mid]>=nums[mid-1]) return mid;
-                else return mid-1;
-            }
-            if(nums[mid]>=nums[mid-1] && nums[mid]>=nums[mid+1]) return mid;
-            else if(nums[mid]<nums[mid-1])
-            {
-                e=mid-1;
+                e=mid;
             }
             else s=mid+1;
         }
