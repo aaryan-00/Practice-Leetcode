bcc03c54bd86fdee1faa798c45cd0b3bffdf2cb1
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-26 16:57:56 +0530

    Time: 27 ms (97.71%), Space: 89.2 MB (79.39%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -5,22 +5,30 @@ public:
         if(n>m)
             return findMedianSortedArrays(nums2,nums1);//ensuring that binary search happens on minimum size array
         
-        int low=0,high=n,medianPos=((m+n)+1)/2;
+        int low=0,high=n,medianPos=(n+m+1)/2;
         while(low<=high) {
             int cut1 = (low+high)>>1;
             int cut2 = medianPos - cut1;
-            int l1,l2,r1,r2;
-            if(cut1==0) l1=INT_MIN;
-            else l1= nums1[cut1-1];
 
-            if(cut2==0) l2=INT_MIN;
-            else l2= nums2[cut2-1];
+            // int l1,l2,r1,r2;
+            // if(cut1==0) l1=INT_MIN;
+            // else l1= nums1[cut1-1];
 
-            if(cut1==n) r1=INT_MAX;
-            else r1= nums1[cut1];
+            // if(cut2==0) l2=INT_MIN;
+            // else l2= nums2[cut2-1];
 
-            if(cut2==m) r2=INT_MAX;
-            else r2= nums2[cut2];
+            // if(cut1==n) r1=INT_MAX;
+            // else r1= nums1[cut1];
+
+            // if(cut2==m) r2=INT_MAX;
+            // else r2= nums2[cut2];
+
+            int l1=INT_MIN,l2=INT_MIN,r1=INT_MAX,r2=INT_MAX;
+            if(cut1!=0)l1=nums1[cut1-1];
+            if(cut2!=0)l2=nums2[cut2-1];
+            if(cut1!=n)r1=nums1[cut1];
+            if(cut2!=m)r2=nums2[cut2];
+
             
             if(l1<=r2 && l2<=r1) {
                 if((m+n)%2 != 0)
