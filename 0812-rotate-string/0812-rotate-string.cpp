1be186c8e37b4b9e1b70dd413970c47fbe6182d5
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-27 16:44:47 +0530

    Time: 4 ms (34.65%), Space: 6 MB (99.71%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        char pivot=goal[0];
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]==pivot)
-            {
-                string curr="";
-                curr+=s.substr(i)+s.substr(0,i);
-                // cout<<curr<<endl;
-                if(curr==goal) return true;
-            }
-        }
-        return false;
+        // char pivot=goal[0];
+        // for(int i=0;i<s.size();i++)
+        // {
+        //     if(s[i]==pivot)
+        //     {
+        //         string curr="";
+        //         curr+=s.substr(i)+s.substr(0,i);
+        //         // cout<<curr<<endl;
+        //         if(curr==goal) return true;
+        //     }
+        // }
+        // return false;
+
+        return s.size()==goal.size() && (s+s).find(goal)!=string::npos;
     }
 };
