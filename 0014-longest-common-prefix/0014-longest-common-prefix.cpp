d26b73d792c1e4b6e8ad5d1a265b8968a62f34ad
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-06-27 16:11:50 +0530

    Time: 9 ms (29.75%), Space: 9 MB (99.95%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -13,17 +13,29 @@ public:
         //     }
         // }
         // return s;
-        int n=s.size();
-        for(int i=1; i< strs.size(); i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if(s[j]!=strs[i][j])
-                {
-                    n=j;
-                }
-            }
+
+
+        // int n=s.size();
+        // for(int i=1; i< strs.size(); i++)
+        // {
+        //     for(int j=0; j<n; j++)
+        //     {
+        //         if(s[j]!=strs[i][j])
+        //         {
+        //             n=j;
+        //         }
+        //     }
+        // }
+        // return s.substr(0,n);
+
+        sort(strs.begin(),strs.end());
+        int a=strs.size();
+        string n=strs[0],m=strs[a-1],ans="";
+        for(int i=0;i<n.size();i++){
+            if(n[i]==m[i]){ans+=n[i];}
+            else break;
         }
-        return s.substr(0,n);
+        return ans;
+        
     }
 };
