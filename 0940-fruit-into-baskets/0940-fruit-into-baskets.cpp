e8b40fe5846bf96b0e962b86f38bb7f5cc0c1e4e
Aaryan <120718690+aaryan-00@users.noreply.github.com>
2023-06-28 17:02:18 +0530

    Time: 110 ms (99.06%), Space: 69 MB (89.27%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& fruits) {
-        int ans=0;
-        int left=0;
-        unordered_map<int,int> mp;
+    // int totalFruit(vector<int>& fruits) {
+    //     int ans=0;
+    //     int left=0;
+    //     unordered_map<int,int> mp;
+    //     for(int i=0;i<fruits.size();i++)
+    //     {
+    //         mp[fruits[i]]++;
+    //         while(mp.size()>2)
+    //         {
+    //             mp[fruits[left]]--;
+    //             if(mp[fruits[left]]==0)
+    //             {
+    //                 mp.erase(fruits[left]);
+    //             }
+    //             left++;
+    //         }
+    //         ans=max(ans,i-left+1);
+    //     }
+        
+    //     return ans;
+    // }
+
+        int totalFruit(vector<int>& fruits) {
+
+        int n=fruits.size();
+        vector<int> hashh(n+1,0);
+        int ans=0,left=0,cnt=0;
         for(int i=0;i<fruits.size();i++)
         {
-            mp[fruits[i]]++;
-            while(mp.size()>2)
+            hashh[fruits[i]]++;
+            if(hashh[fruits[i]]==1) cnt++;
+            while(cnt>2)
             {
-                mp[fruits[left]]--;
-                if(mp[fruits[left]]==0)
+                hashh[fruits[left]]--;
+                if(hashh[fruits[left]]==0)
                 {
-                    mp.erase(fruits[left]);
+                    cnt--;
                 }
                 left++;
             }
             ans=max(ans,i-left+1);
         }
+        
         return ans;
     }
 };
