b2b7812e76c03d5035383b3b35d885babb895ab7
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-01 22:27:37 +0530

    Time: 113 ms (5.03%), Space: 57.1 MB (6.73%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -1,27 +1,48 @@
 class Solution {
 public:
+    // int longestOnes(vector<int>& nums, int k) {
+    //     vector<int> pos;
+    //     int ans=0;
+    //     int cnt=0;
+    //     int start=0;
+    //     int iter=0;
+    //     for(int i=0;i<nums.size();i++)
+    //     {
+    //         if(nums[i]==0)
+    //         {
+    //             pos.push_back(i);
+    //             cnt++;
+    //         }
+    //         if(cnt>k)
+    //         {
+    //             start=pos[iter]+1;
+    //             iter++;
+    //             cnt--;
+    //         }
+    //         ans=max(ans,i-start+1);
+    //     }
+    //     return ans;
+    // }
+
     int longestOnes(vector<int>& nums, int k) {
-        vector<int> pos;
+        priority_queue<int,vector<int>,greater<int>> pq;
         int ans=0;
         int cnt=0;
         int start=0;
-        int iter=0;
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]==0)
             {
-                pos.push_back(i);
+                pq.push(i);
                 cnt++;
             }
-            // cout<<pos.size()<<" "<<cnt<<" ";
             if(cnt>k)
             {
-                start=pos[iter]+1;
-                iter++;
+                start=pq.top()+1;
+                pq.pop();
                 cnt--;
             }
             ans=max(ans,i-start+1);
-            // cout<<ans<<endl;
         }
         return ans;
     }
