b4f86fd5a2a070b5529ac556289fcc7fb553c20b
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-01 21:57:51 +0530

    Time: 8 ms (88.5%), Space: 8.2 MB (74.21%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
+    // int lengthOfLongestSubstring(string s) {
+    //     unordered_map<char,int> mp;
+    //     int start=0,cnt=0,ans=0;
+    //     for(int i=0;i<s.size();i++)
+    //     {
+    //         mp[s[i]]++;
+    //         cnt++;
+    //         if(mp.size()==cnt)
+    //         {
+    //             ans=max(ans,cnt);
+    //         }
+    //         while(mp.size()!=cnt)
+    //         {
+    //             if(mp[s[start]]==1)
+    //             {
+    //                 mp.erase(s[start]);
+    //             }
+    //             else
+    //             {
+    //                 mp[s[start]]--;
+    //             }
+    //             start++;
+    //             cnt--;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int> mp;
-        int start=0,cnt=0,ans=0;
-        for(int i=0;i<s.size();i++)
-        {
-            mp[s[i]]++;
-            cnt++;
-            if(mp.size()==cnt)
-            {
-                ans=max(ans,cnt);
-            }
-            while(mp.size()!=cnt)
-            {
-                if(mp[s[start]]==1)
-                {
-                    mp.erase(s[start]);
-                }
-                else
-                {
-                    mp[s[start]]--;
-                }
-                start++;
-                cnt--;
-            }
-        }
-        return ans;
+      vector < int > mpp(256, -1);
+      int left = 0, right = 0;
+      int n = s.size();
+      int len = 0;
+      while (right < n) {
+        if (mpp[s[right]] != -1)
+          left = max(mpp[s[right]] + 1, left);
+
+        mpp[s[right]] = right;
+
+        len = max(len, right - left + 1);
+        right++;
+      }
+      return len;
     }
 };
