64e013f31953d41aa35686969c02149dce2e6a63
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-02 00:20:59 +0530

    Time: 4 ms (96.43%), Space: 7 MB (35.89%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    // int characterReplacement(string s, int k) {
+    //     vector<int> mp(26,0);
+    //     int start=0,maxFreq=0,ans=0;
+    //     for(int i=0;i<s.size();i++)
+    //     {
+    //         int curr=s[i]-'A';
+    //         mp[curr]++;
+    //         maxFreq=max(maxFreq,mp[curr]);
+    //         while(i-start+1 - maxFreq>k)
+    //         {
+    //             mp[s[start]-'A']--;
+    //             start++;
+    //             // cout<<maxFreq<<endl;
+    //             maxFreq=0;
+    //             for(int i=0;i<26;i++)
+    //             {
+    //                 maxFreq=max(maxFreq,mp[i]);
+    //             }
+    //         }
+    //         ans=max(ans,i-start+1);
+
+    //     }
+    //     return ans;
+    // }
+
     int characterReplacement(string s, int k) {
         vector<int> mp(26,0);
         int start=0,maxFreq=0,ans=0;
@@ -8,16 +33,10 @@ public:
             int curr=s[i]-'A';
             mp[curr]++;
             maxFreq=max(maxFreq,mp[curr]);
-            while(i-start+1 - maxFreq>k)
+            if(i-start+1 - maxFreq>k)
             {
                 mp[s[start]-'A']--;
                 start++;
-                // cout<<maxFreq<<endl;
-                maxFreq=0;
-                for(int i=0;i<26;i++)
-                {
-                    maxFreq=max(maxFreq,mp[i]);
-                }
             }
             ans=max(ans,i-start+1);
 
