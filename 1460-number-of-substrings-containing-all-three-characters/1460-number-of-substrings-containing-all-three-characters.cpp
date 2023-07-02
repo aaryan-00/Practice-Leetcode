394f9c15c4a440cea535d6c4fe7c39ad5b42dd9a
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-02 15:40:47 +0530

    Time: 88 ms (34.41%), Space: 8.5 MB (42.16%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -14,14 +14,27 @@ public:
                 mp[s[start]]--;
                 start++;
             }
-            // int j=i+1;
-            // while(j<n && mp.size()<3)
-            // {
-            //     mp[s[j]]++;
-            //     j++;
-            // }
-            // if(mp.size()==3) ans+=(n-j+1);
         }
         return ans;
     }
+
+
+    // int numberOfSubstrings(string s) {
+    //     int ans=0;
+    //     int n=s.size();
+    //     int start=0;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         vector<int> mp(3,0);
+    //         mp[s[i]-'a']++;
+    //         int j=i+1;
+    //         while(j<n && (mp[0]<1 || mp[1]<1 || mp[2]<1))
+    //         {
+    //             mp[s[j]-'a']++;
+    //             j++;
+    //         }
+    //         if(mp.size()==3) ans+=(n-j+1);
+    //     }
+    //     return ans;
+    // }
 };
