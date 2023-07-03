a2049b6da5e20ac72c380dc7e3398cae968bc616
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-03 22:10:45 +0530

    Time: 467 ms (98.26%), Space: 128 MB (98.77%) - LeetHub

--- 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.cpp
+++ 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.cpp
@@ -63,32 +63,89 @@ public:
 
 
 //here we marked restricted node visited, so there is no going to their subtrees
-    int ans=0;
-    void dfs(int node,vector<int> adj[],vector<bool> &vis)
+    // int ans=0;
+    // void dfs(int node,vector<int> adj[],vector<bool> &vis)
+    // {
+    //     ans++;
+    //     vis[node]=true;
+    //     for(auto i:adj[node])
+    //     {
+    //         if(!vis[i])
+    //         {
+    //             dfs(i,adj,vis);
+    //         }
+    //     }
+    // }
+    // int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
+    //     vector<int> adj[n];
+    //     for(auto i:edges)
+    //     {
+    //         adj[i[0]].push_back(i[1]);
+    //         adj[i[1]].push_back(i[0]);
+    //     }
+    //     vector<bool> vis(n,false);
+    //     for(auto i:restricted)
+    //     {
+    //         vis[i]=true;
+    //     }
+    //     dfs(0,adj,vis);
+    //     return ans;
+    // }
+
+    vector<int> par;
+    vector<int> sizes;
+    void make(int v)
     {
-        ans++;
-        vis[node]=true;
-        for(auto i:adj[node])
+        par[v] = v;
+        sizes[v] = 1;
+    }
+    int find(int v)
+    {
+        if (par[v] == v)
+            return v;
+        return par[v] = find(par[v]);
+    }
+    void unionSet(int a, int b)
+    {
+        a = find(a);
+        b = find(b);
+        if (a != b)
         {
-            if(!vis[i])
+            if (sizes[b] > sizes[a])
             {
-                dfs(i,adj,vis);
+                swap(b, a);
             }
+            if(b==0)
+            {
+                swap(a,b);
+            }
+            par[b] = a;
+            sizes[a] += sizes[b];
         }
     }
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
-        vector<int> adj[n];
-        for(auto i:edges)
-        {
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
-        }
-        vector<bool> vis(n,false);
+        par.resize(n);
+        sizes.resize(n);
+        vector<bool> res(n,false);
         for(auto i:restricted)
         {
-            vis[i]=true;
+            res[i]=true;
         }
-        dfs(0,adj,vis);
-        return ans;
+        for(int i=0;i<n;i++)
+        {
+            make(i);
+        }
+        for(auto i:edges)
+        {
+            if(res[i[0]]==false && res[i[1]]==false)
+            {
+                unionSet(i[0],i[1]);
+            }
+        }
+        // cout<<sizes[1]<<" "<<sizes[0]<<endl;
+        return sizes[0];
     }
+
+
+
 };
