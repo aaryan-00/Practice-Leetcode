96dbff6f6ce101505fb04c52da9c178a95555e09
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-03 21:36:53 +0530

    Time: 147 ms (35.65%), Space: 46.9 MB (46.01%) - LeetHub

--- 0881-loud-and-rich/0881-loud-and-rich.cpp
+++ 0881-loud-and-rich/0881-loud-and-rich.cpp
@@ -1,10 +1,40 @@
 class Solution {
 public:
+    // int dfs(int i,vector<int> &quiet,vector<vector<int>>& adj,vector<int> &ans)
+    // {
+    //     if(ans[i]>=0) return ans[i];
+
+    //     ans[i]=i;
+    //     for(auto it:adj[i])
+    //     {
+    //         int ind=dfs(it,quiet,adj,ans);
+    //         if(quiet[ans[i]]>quiet[ind])
+    //         {
+    //             // ans[i]=ind;
+    //             ans[i]=ans[it];
+    //         }
+    //     }
+    //     return ans[i];
+    // }
+    // vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+    //     int n=quiet.size();
+    //     vector<vector<int>> adj(n);
+    //     for(auto i:richer)
+    //     {
+    //         adj[i[1]].push_back(i[0]);
+    //     }
+    //     vector<int> ans(n,-1);
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         dfs(i,quiet,adj,ans);
+    //     }
+    //     return ans;
+    // }
+
+
     int dfs(int i,vector<int> &quiet,vector<vector<int>>& adj,vector<int> &ans)
     {
-        if(ans[i]>=0) return ans[i];
-
-        ans[i]=i;
+        if(ans[i]!=i) return ans[i];
         for(auto it:adj[i])
         {
             int ind=dfs(it,quiet,adj,ans);
@@ -23,10 +53,15 @@ public:
         {
             adj[i[1]].push_back(i[0]);
         }
-        vector<int> ans(n,-1);
+        vector<int> ans(n);
+        for(int i=0;i<n;i++)
+        {
+            ans[i]=i;
+        }
         for(int i=0;i<n;i++)
         {
             dfs(i,quiet,adj,ans);
+            
         }
         return ans;
     }
