e4e61e57f739fb60479fa894f47ed76add3ae622
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-03 21:49:24 +0530

    Time: 777 ms (20.08%), Space: 195.2 MB (26.12%) - LeetHub

--- 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.cpp
+++ 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.cpp
@@ -1,15 +1,45 @@
 class Solution {
 public:
+    // int ans=0;
+    // void dfs(int node,vector<int> adj[],vector<bool> &vis,set<int> &s)
+    // {
+    //     ans++;
+    //     vis[node]=true;
+    //     for(auto i:adj[node])
+    //     {
+    //         if(!vis[i] && s.count(i)==0)
+    //         {
+    //             dfs(i,adj,vis,s);
+    //         }
+    //     }
+    // }
+    // int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
+    //     vector<int> adj[n];
+    //     for(auto i:edges)
+    //     {
+    //         adj[i[0]].push_back(i[1]);
+    //         adj[i[1]].push_back(i[0]);
+    //     }
+    //     set<int> s;
+    //     vector<bool> vis(n,false);
+    //     for(auto i:restricted)
+    //     {
+    //         s.insert(i);
+    //     }
+    //     dfs(0,adj,vis,s);
+    //     return ans;
+    // }
+
+
     int ans=0;
-    void dfs(int node,vector<int> adj[],vector<bool> &vis,set<int> &s)
+    void dfs(int node,int par,vector<int> adj[],set<int> &s)
     {
         ans++;
-        vis[node]=true;
         for(auto i:adj[node])
         {
-            if(!vis[i] && s.count(i)==0)
+            if(i!=par && s.count(i)==0)
             {
-                dfs(i,adj,vis,s);
+                dfs(i,node,adj,s);
             }
         }
     }
@@ -21,13 +51,11 @@ public:
             adj[i[1]].push_back(i[0]);
         }
         set<int> s;
-        vector<bool> vis(n,false);
         for(auto i:restricted)
         {
             s.insert(i);
         }
-        dfs(0,adj,vis,s);
+        dfs(0,-1,adj,s);
         return ans;
-
     }
 };
