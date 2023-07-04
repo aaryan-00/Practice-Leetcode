ba60744ec44e4d18392340de8011feb39099e135
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-04 23:33:22 +0530

    Time: 7 ms (60.82%), Space: 12.4 MB (8.36%) - LeetHub

---
+++ 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    vector<int> par;
+    vector<int> sizes;
+    void make(int v)
+    {
+        par[v] = v;
+        sizes[v] = 1;
+    }
+    int find(int v)
+    {
+        if (par[v] == v)
+            return v;
+        return par[v] = find(par[v]);
+    }
+    void unionSet(int a, int b)
+    {
+        a = find(a);
+        b = find(b);
+        if (a != b)
+        {
+            if (sizes[b] > sizes[a])
+            {
+                swap(b, a);
+            }
+            par[b] = a;
+            sizes[a] += sizes[b];
+        }
+    }
+    bool equationsPossible(vector<string>& equations) {
+        par.resize(26);
+        sizes.resize(26);
+        for(int i=0;i<26;i++)
+        {
+            make(i);
+        }
+        vector<string> equals;
+        vector<string> unequals;
+        for(auto i:equations)
+        {
+            if(i[1]=='=') equals.push_back(i);
+            else unequals.push_back(i);
+        }
+        for(auto i:equals)
+        {
+            int a=i[0]-'a',b=i[3]-'a';
+            unionSet(a,b);
+        }
+        for(auto i:unequals)
+        {
+            int a=i[0]-'a',b=i[3]-'a';
+            int par_a=find(a);
+            int par_b=find(b);
+            if(par_a==par_b) return false;
+        }
+        return true;
+    }
+};
