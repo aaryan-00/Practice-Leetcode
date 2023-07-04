9ecfb27025df2da931f616ac6516fda3677233f6
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-04 22:38:36 +0530

    Time: 169 ms (44.44%), Space: 47.3 MB (44.55%) - LeetHub

--- 1120-flower-planting-with-no-adjacent/1120-flower-planting-with-no-adjacent.cpp
+++ 1120-flower-planting-with-no-adjacent/1120-flower-planting-with-no-adjacent.cpp
@@ -1,5 +1,53 @@
 class Solution {
 public:
+    // vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
+    //     vector<int> adj[n];
+    //     for(auto i:paths)
+    //     {
+    //         int u=i[0]-1,v=i[1]-1;
+    //         adj[u].push_back(v);
+    //         adj[v].push_back(u);
+    //     }
+    //     vector<int> color(n,-1);
+    //     queue<int> q;
+    //     for(int ind=0;ind<n;ind++)
+    //     {
+    //         if(color[ind]==-1)
+    //         {
+    //             q.push(ind);
+    //             while(!q.empty())
+    //             {
+    //                 int curr=q.front();
+    //                 q.pop();
+    //                 vector<int> temp(5,0);
+    //                 for(auto i:adj[curr])
+    //                 {
+    //                     if(color[i]==-1)
+    //                     {
+    //                         q.push(i);
+    //                     }
+    //                     else
+    //                     {
+    //                         int col=color[i];
+    //                         temp[col]=1;
+    //                     }
+    //                 }
+    //                 for(int i=1;i<5;i++)
+    //                 {
+    //                     if(temp[i]==0)
+    //                     {
+    //                         color[curr]=i;
+    //                         break;
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return color;
+    // }
+
+
+
     vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
         vector<int> adj[n];
         for(auto i:paths)
@@ -9,12 +57,14 @@ public:
             adj[v].push_back(u);
         }
         vector<int> color(n,-1);
+        vector<int> vis(n,0);
         queue<int> q;
         for(int ind=0;ind<n;ind++)
         {
-            if(color[ind]==-1)
+            if(!vis[ind])
             {
                 q.push(ind);
+                vis[ind]=1;
                 while(!q.empty())
                 {
                     int curr=q.front();
@@ -22,11 +72,7 @@ public:
                     vector<int> temp(5,0);
                     for(auto i:adj[curr])
                     {
-                        if(color[i]==-1)
-                        {
-                            q.push(i);
-                        }
-                        else
+                        if(color[i]!=-1)
                         {
                             int col=color[i];
                             temp[col]=1;
@@ -40,6 +86,15 @@ public:
                             break;
                         }
                     }
+                    for(auto i:adj[curr])
+                    {
+                        if(!vis[i])
+                        {
+                            q.push(i);
+                            vis[i]=1;
+                        }
+                    }
+
                 }
             }
         }
