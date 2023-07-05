1d483937f6c18420352392677ea2bafdb6a2df16
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-05 22:51:23 +0530

    Time: 229 ms (80.37%), Space: 68.8 MB (37.3%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.cpp
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -1,14 +1,50 @@
 class Solution {
 public:
-    void dfs(vector<int> adj[],int src,vector<bool> &vis,int ind,vector<set<int>> &check)
+    // void dfs(vector<int> adj[],int src,vector<bool> &vis,int ind,vector<set<int>> &check)
+    // {
+    //     vis[src]=true;
+    //     for(auto i:adj[src])
+    //     {
+    //         if(!vis[i])
+    //         {
+    //             check[ind].insert(i);
+    //             dfs(adj,i,vis,ind,check);
+    //         }
+    //     }
+    // }
+    // vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+    //     int n=numCourses;
+    //     vector<int> adj[n];
+    //     for(auto i:prerequisites)
+    //     {
+    //         adj[i[0]].push_back(i[1]);
+    //     }
+    //     vector<set<int>> check(n);
+    //     set<pair<int,vector<int>>> s;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         vector<bool> vis(n,0);
+    //         dfs(adj,i,vis,i,check);
+
+    //     }
+    //     vector<bool> ans;
+    //     for(auto i:queries)
+    //     {
+    //         if(check[i[0]].count(i[1])) ans.push_back(1);
+    //         else ans.push_back(0);
+    //     }
+    //     return ans;
+    // }
+    vector<vector<int>> mat;
+    void dfs(vector<int> adj[],int src,vector<bool> &vis,int ind)
     {
         vis[src]=true;
+        mat[ind][src]=true;
         for(auto i:adj[src])
         {
             if(!vis[i])
             {
-                check[ind].insert(i);
-                dfs(adj,i,vis,ind,check);
+                dfs(adj,i,vis,ind);
             }
         }
     }
@@ -19,19 +55,17 @@ public:
         {
             adj[i[0]].push_back(i[1]);
         }
-        vector<set<int>> check(n);
-        set<pair<int,vector<int>>> s;
+        mat.resize(n,vector<int>(n,0));
         for(int i=0;i<n;i++)
         {
             vector<bool> vis(n,0);
-            dfs(adj,i,vis,i,check);
+            dfs(adj,i,vis,i);
 
         }
         vector<bool> ans;
         for(auto i:queries)
         {
-            if(check[i[0]].count(i[1])) ans.push_back(1);
-            else ans.push_back(0);
+            ans.push_back(mat[i[0]][i[1]]);
         }
         return ans;
     }
