956dd88ad9023ce130c7dfb548376d64fc572fd1
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-07 18:36:33 +0530

    Time: 216 ms (50.31%), Space: 100.5 MB (62.27%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
@@ -1,38 +1,62 @@
 class Solution {
 public:
-    void bfs(vector<int> adj[],int src,vector<int> &dis)
+    // void bfs(vector<int> adj[],int src,vector<int> &dis)
+    // {
+    //     queue<int> q;
+    //     q.push(src);
+    //     dis[src]=0;
+    //     while(!q.empty())
+    //     {
+    //         int node=q.front();
+    //         q.pop();
+    //         for(auto it:adj[node])
+    //         {
+    //             if(dis[it]==-1)
+    //             {
+    //                 dis[it]=dis[node]+1;
+    //                 q.push(it);
+    //             }
+    //         }
+    //     }
+    // }
+    void bfs2(vector<int>& edges,int src,vector<int> &dis)
     {
         queue<int> q;
         q.push(src);
         dis[src]=0;
         while(!q.empty())
         {
-            int node=q.front();
+            int u=q.front();
             q.pop();
-            for(auto it:adj[node])
+            if(edges[u]!=-1 )
             {
-                if(dis[it]==-1)
+                if(dis[edges[u]]==-1)
                 {
-                    dis[it]=dis[node]+1;
-                    q.push(it);
-                }
+                    dis[edges[u]]=dis[u]+1;
+                    q.push(edges[u]);
+                }    
             }
         }
     }
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n=edges.size();
-        vector<int> adj[n];
-        for(int i=0;i<n;i++)
-        {
-            if(edges[i]==-1) continue;
-            int u=i;
-            int v=edges[i];
-            adj[u].push_back(v);
-        }
+        // vector<int> adj[n];
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(edges[i]==-1) continue;
+        //     int u=i;
+        //     int v=edges[i];
+        //     adj[u].push_back(v);
+        // }
+        // vector<int> dis1(n,-1);
+        // bfs(adj,node1,dis1);
+        // vector<int> dis2(n,-1);
+        // bfs(adj,node2,dis2);
+
         vector<int> dis1(n,-1);
-        bfs(adj,node1,dis1);
+        bfs2(edges,node1,dis1);
         vector<int> dis2(n,-1);
-        bfs(adj,node2,dis2);
+        bfs2(edges,node2,dis2);
         // for(auto i:dis1)
         // {
         //     cout<<i<<" ";
@@ -46,7 +70,6 @@ public:
         int node=-1;
         for(int i=0;i<n;i++)
         {
-            // int curr=INT_MIN;
             if(dis1[i]!=-1 && dis2[i]!=-1)
             {
                 int curr=max(dis1[i],dis2[i]);
