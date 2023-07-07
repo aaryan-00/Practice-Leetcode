236131dac83c8dd4955a8cd8b76704ca04bd8510
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-08 00:14:23 +0530

    Time: 654 ms (75.63%), Space: 152 MB (79.2%) - LeetHub

--- 1912-number-of-restricted-paths-from-first-to-last-node/1912-number-of-restricted-paths-from-first-to-last-node.cpp
+++ 1912-number-of-restricted-paths-from-first-to-last-node/1912-number-of-restricted-paths-from-first-to-last-node.cpp
@@ -1,74 +1,34 @@
 class Solution {
 public:
-    const int m=1e9+7;
-    vector<long> dijkstra(int n,vector<vector<int>> adj[],int src)
-    {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<long> dis(n, INT_MAX);
-        pq.push({0, src});
-        dis[src] = 0;
-        while(!pq.empty())
-        {
-            pair<int,int> curr=pq.top();
-            pq.pop();
-            int Dis=curr.first;
-            int node=curr.second;
-            if(Dis>dis[node]) continue;
-            for(auto i:adj[node])
-            {
-                int next=i[0];
-                int next_dis=i[1];
-                if(dis[next]>dis[node]+next_dis)
-                {
-                    dis[next]=dis[node]+next_dis;
-                    pq.push({dis[next],next});
-                }
-            }   
-        }
-        return dis;
-
+int dfs(vector<vector<pair<int, int>>> &al, vector<int> &dist, vector<int> &dp, int i) {
+    if (i == 1)
+        return 1;
+    if (dp[i] == -1) {
+        dp[i] = 0;
+        for (auto [j, w] : al[i])
+            if (dist[i] < dist[j])
+                dp[i] = (dp[i] + dfs(al, dist, dp, j)) % 1000000007;
     }
-    vector<long> cnt;
-    // int ans=0;
-    int dfs(vector<vector<int>> adj[],int src,vector<long> &dis)
-    {
-        // if(src==dis.size()-1) ans++;
-        // ans%=m;
-        if(src==0) return 1;
-        if(cnt[src]!=-1) return cnt[src];
-        long curr=0;
-        for(auto i:adj[src])
-        {
-            long node=i[0];
-            if(dis[node]>dis[src])
-            {
-                // cnt[node]+=1;
-                // cnt[node]%=m;
-                curr=(curr+dfs(adj,node,dis))%m;
+    return dp[i];
+}
+int countRestrictedPaths(int n, vector<vector<int>>& edges) {
+    vector<vector<pair<int, int>>> al(n + 1);
+    vector<int> dist(n + 1), dp(n + 1, -1);
+    for (auto &e : edges) {
+        al[e[0]].push_back({e[1], e[2]});
+        al[e[1]].push_back({e[0], e[2]});
+    }
+    priority_queue<pair<int, int>, vector<pair<int, int>>> pq;
+    pq.push({0, n});
+    while (!pq.empty()) {
+        auto i = pq.top().second; pq.pop();
+        for (auto [j, w] : al[i]) {
+            if (j != n && (dist[j] == 0 || dist[j] > dist[i] + w)) {
+                dist[j] = dist[i] + w;
+                pq.push({-dist[j], j});
             }
         }
-        return cnt[src]=curr%m;
     }
-    int countRestrictedPaths(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj[n];
-        for(auto i:edges)
-        {
-            int u=i[0]-1;
-            int v=i[1]-1;
-            int wt=i[2];
-            adj[u].push_back({v,wt});
-            adj[v].push_back({u,wt});
-        }
-        vector<long> dis=dijkstra(n, adj,n-1);
-        cnt.resize(n,-1);
-        // cnt[0]=1;
-        dfs(adj,n-1,dis);
-        // for(auto i:cnt)
-        // {
-        //     cout<<i<<" ";
-        // }
-        // cout<<endl;
-        return cnt[n-1];
-        
-    }
+    return dfs(al, dist, dp, n);
+}
 };
