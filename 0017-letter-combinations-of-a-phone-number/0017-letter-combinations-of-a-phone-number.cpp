92de04e00d85731372b5045a0f0e44c7ac7f7189
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-11 00:24:33 +0530

    Time: 5 ms (9.22%), Space: 6.5 MB (32.52%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -3,28 +3,46 @@ public:
     vector<string> info={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     vector<string> ans;
     string temp;
+    // void combi(int ind,string &digits)
+    // {
+    //     if(ind==digits.size() && temp.size()==digits.size())
+    //     {
+    //         ans.push_back(temp);
+    //         return;
+    //     }
+    //     for(int i=ind;i<digits.size();i++)
+    //     {
+    //         int curr=digits[i]-'0';
+    //         // cout<<curr<<endl;
+    //         string s=info[curr];
+    //         for(int j=0;j<s.size();j++)
+    //         {
+    //             temp+=s[j];
+    //             combi(i+1,digits);
+    //             temp.pop_back();
+    //         }
+    //     }
+    // }
+
     void combi(int ind,string &digits)
     {
-        if(ind==digits.size() && temp.size()==digits.size() && temp.size()!=0)
+        if(ind==digits.size())
         {
             ans.push_back(temp);
             return;
         }
-        for(int i=ind;i<digits.size();i++)
+        int curr=digits[ind]-'0';
+        string s=info[curr];
+        for(int j=0;j<s.size();j++)
         {
-            int curr=digits[i]-'0';
-            // cout<<curr<<endl;
-            string s=info[curr];
-            for(int j=0;j<s.size();j++)
-            {
-                temp+=s[j];
-                combi(i+1,digits);
-                temp.pop_back();
-            }
+            temp+=s[j];
+            combi(ind+1,digits);
+            temp.pop_back();
         }
     }
     
     vector<string> letterCombinations(string digits) {
+        if(digits.size()==0) return {};
         ans.clear();
         // temp.clear();
         combi(0,digits);
