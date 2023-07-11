4f438aa2a5c2debaf023f9fbb4e6fb7244a36a57
aaryan-00 <120718690+aaryan-00@users.noreply.github.com>
2023-07-11 17:31:43 +0530

    Time: 0 ms (100%), Space: 7.6 MB (39.88%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<vector<string>> ans;
     vector<string> temp;
+    vector<bool> rowCheck,upDiagonal,downDiagonal;
     bool isSafe(int col,int row,int n)
     {
         int r=row,c=col;
@@ -30,17 +31,30 @@ public:
         }
         for(int row=0;row<n;row++)
         {
-            if(isSafe(col,row,n))
+            // if(isSafe(col,row,n))
+            // {
+            //     temp[row][col]='Q';
+            //     solve(col+1,n);
+            //     temp[row][col]='.';
+            // }
+
+            // or
+            if(rowCheck[row]==0 && downDiagonal[row+col]==0 && upDiagonal[n-1+row-col]==0)
             {
+                rowCheck[row]=1, downDiagonal[row+col]=1, upDiagonal[n-1+row-col]=1;
                 temp[row][col]='Q';
                 solve(col+1,n);
                 temp[row][col]='.';
+                rowCheck[row]=0, downDiagonal[row+col]=0, upDiagonal[n-1+row-col]=0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         ans.clear();
         temp.resize(n);
+        rowCheck.resize(n,false);
+        upDiagonal.resize(2*n-1,false);
+        downDiagonal.resize(2*n-1,false);
         string curr(n,'.');
         for(auto &i:temp)
         {
