f0594b6373f4486222cbffc3bf37af739bb54f2e
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-07-26 17:55:25 +0530

    Time: 46 ms (18.57%), Space: 16.3 MB (34.81%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    // turn==0 for alice and 1 for bob
-    bool getWinner(vector<int> &piles,int turn,int start,int end,int alice,int bob)
+    // turn==1 for alice and 0 for bob
+    bool getWinner(vector<int> &piles,int turn,int start,int end,vector<vector<int>> &dp)
     {
-        if(start<=end)
+        if(start>end) return 0;
+        if(start==end) return piles[start];
+        if(dp[start][end]!=-1) return dp[start][end];
+            
+        if(turn)
         {
-            if(turn==1)
-            {
-                return getWinner(piles,!turn,start+1,end,alice,bob+piles[start])||getWinner(piles,!turn,start,end-1,alice,bob+piles[end]);
-            }
-            else if(turn==0)
-            {
-                return getWinner(piles,!turn,start+1,end,alice+piles[start],bob)||getWinner(piles,!turn,start,end-1,alice+piles[end],bob);
-            } 
+            dp[start][end]=max(piles[start]+getWinner(piles,!turn,start+1,end,dp),piles[end]+getWinner(piles,!turn,start,end-1,dp));
         }
-        return alice>bob;
-
-        
+        else
+        {
+            dp[start][end]=min(-piles[start]+getWinner(piles,!turn,start+1,end,dp),-piles[end]+getWinner(piles,!turn,start,end-1,dp));
+        } 
+        return dp[start][end];
     }
     bool stoneGame(vector<int>& piles) {
-        // return getWinner(piles,0,0,piles.size()-1,0,0);
-        return true;
+        int n=piles.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return getWinner(piles,1,0,piles.size()-1,dp)>0;
     }
 };
