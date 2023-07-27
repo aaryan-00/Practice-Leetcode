af8da9d38159fa33aa0f88773c65a0c400e994c5
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-07-28 00:31:11 +0530

    Time: 707 ms (66.5%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,9 +1,2 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 SELECT name, population, area FROM World WHERE area>=3000000 OR population>=25000000;
-# select area,population,name
-# from world
-# where area>=3000000
-# union
-# select area,population,name
-# from world
-# where population>=25000000;
