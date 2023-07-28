bfb8ec66195681a912334c134d346aa516ea28ce
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-07-28 18:52:04 +0530

    Time: 34 ms (85.38%), Space: 20.6 MB (63%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,11 +1,46 @@
 class Solution {
 public:
+    // vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    //     unordered_map<string,vector<string>> mp;
+    //     for(auto i:strs)
+    //     {
+    //         string curr=i;
+    //         sort(curr.begin(),curr.end());
+    //         mp[curr].push_back(i);
+    //     }
+    //     vector<vector<string>> ans;
+    //     for(auto i:mp)
+    //     {
+    //         ans.push_back(i.second);
+    //     }
+    //     return ans;
+    // }
+
+
+    string sortLowercase(string s) {
+        int charExist[26] = {0};
+        for (int i = 0; i < s.size(); i++) {
+            charExist[s[i] - 'a']++;
+        }
+        string res;
+        int j = 0;
+        while (j < 26) {
+            if (charExist[j] == 0) {
+                j++;
+            }
+            else {
+                res.push_back(j + 'a');
+                charExist[j]--;
+            }
+        }
+        return res;
+    }
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> mp;
         for(auto i:strs)
         {
-            string curr=i;
-            sort(curr.begin(),curr.end());
+            string curr=sortLowercase(i);
             mp[curr].push_back(i);
         }
         vector<vector<string>> ans;
