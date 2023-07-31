6c8ff35c4460f55b3ad279b087727a03be3090be
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-07-31 14:42:15 +0530

    Time: 2224 ms (41.64%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
+# select product_name,year,price
+# from Product, Sales
+# where Product.product_id=Sales.product_id;
+
 select product_name,year,price
-from Product, Sales
-where Product.product_id=Sales.product_id;
+from Product join Sales
+on Product.product_id=Sales.product_id;
