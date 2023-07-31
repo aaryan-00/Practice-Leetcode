c2c11fc2cefded082125f43b078237bba32857c4
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-07-31 14:43:12 +0530

    Time: 1748 ms (95.24%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -3,6 +3,11 @@
 # from Product, Sales
 # where Product.product_id=Sales.product_id;
 
-select product_name,year,price
-from Product join Sales
-on Product.product_id=Sales.product_id;
+# select product_name,year,price
+# from Product join Sales
+# on Product.product_id=Sales.product_id;
+
+
+select p.product_name,s.year,s.price
+from Product as p join Sales as s
+on p.product_id=s.product_id;
