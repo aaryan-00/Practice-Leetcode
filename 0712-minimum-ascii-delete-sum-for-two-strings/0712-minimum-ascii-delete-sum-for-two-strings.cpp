1674b37b2d25f171d1a7dded28b38d83324f5082
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-07-31 15:49:13 +0530

    Time: 414 ms (5.11%), Space: 15.2 MB (68.25%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int minimumDeleteSum(string s, string t) {
+        int n=s.size(),m=t.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0||j==0)
+                {
+                    dp[i][j]=0;
+                }
+                else
+                {
+                    if(s[i-1]==t[j-1])
+                    {
+                        dp[i][j]=(int)s[i-1]+dp[i-1][j-1];
+                    }
+                    else
+                    {
+                        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        int ans=0;
+        for(auto i:s)
+        {
+            ans+=((int)i);
+        }
+        // ans-=dp[n][m];
+        // cout<<ans<<endl;
+        for(auto i:t)
+        {
+            ans+=((int)i);
+        }
+        return ans-2*dp[n][m];
+    }
+};
