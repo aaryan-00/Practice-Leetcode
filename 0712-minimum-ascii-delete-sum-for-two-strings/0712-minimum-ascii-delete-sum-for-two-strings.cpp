251b5de02535d67410816efd5b2cfe6e28904621
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-07-31 15:52:28 +0530

    Time: 34 ms (83.67%), Space: 15.3 MB (14.89%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -3,24 +3,17 @@ public:
     int minimumDeleteSum(string s, string t) {
         int n=s.size(),m=t.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++)
+        for(int i=1;i<=n;i++)
         {
-            for(int j=0;j<=m;j++)
+            for(int j=1;j<=m;j++)
             {
-                if(i==0||j==0)
+                if(s[i-1]==t[j-1])
                 {
-                    dp[i][j]=0;
+                    dp[i][j]=(int)s[i-1]+dp[i-1][j-1];
                 }
                 else
                 {
-                    if(s[i-1]==t[j-1])
-                    {
-                        dp[i][j]=(int)s[i-1]+dp[i-1][j-1];
-                    }
-                    else
-                    {
-                        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                    }
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
