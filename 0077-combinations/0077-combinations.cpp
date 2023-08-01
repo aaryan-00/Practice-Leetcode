f9ff83083e696c2f8ad6549ea272fb3d54b928cc
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-01 13:01:05 +0530

    Time: 16 ms (88.92%), Space: 9.8 MB (56.99%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> temp;
+    void getCombi(int ind,int n,int k)
+    {
+        // if(ind>n) return;
+        if(temp.size()==k)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=ind;i<=n;i++)
+        {
+            temp.push_back(i);
+            getCombi(i+1,n,k);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        ans.clear();
+        temp.clear();
+        getCombi(1,n,k);
+        return ans;
+    }
+};
