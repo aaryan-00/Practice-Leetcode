36c357a4f1eecb5ee9f3375b9c87d6d937477aef
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-05 11:35:48 +0530

    Time: 0 ms (100%), Space: 6 MB (73.08%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
+    int dp[20];
     int getCount(int i)
     {
         if(i==0||i==1) return 1;
+        if(dp[i]!=-1) return dp[i];
         int cnt=0;
         for(int curr=0;curr<i;curr++)
         {
             cnt+=getCount(curr)*getCount(i-curr-1);
         }
         // cout<<cnt<<endl;
-        return cnt;
+        return dp[i]=cnt;
     }
     int numTrees(int n) {
+        memset(dp,-1,sizeof(dp));
         return getCount(n);
     }
 };
