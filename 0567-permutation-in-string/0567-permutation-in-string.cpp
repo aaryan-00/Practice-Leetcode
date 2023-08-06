2bfe0227c44cba1caeeb3751529252c0eb0fda8d
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-06 15:13:08 +0530

    Time: 9 ms (59.4%), Space: 7.3 MB (60.22%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) return false;
         vector<int> hashh(26,0);
         for(auto i:s1)
         {
             hashh[i-'a']++;
-            // cout<<i-'a'<<endl;
         }
-        int k=s1.size()-1;
-        int start=0;
-        for(int i=k;i<s2.size();i++)
-        {
-            vector<int> check(26,0);
-            for(int j=start;j<=i;j++)
+        vector<int> check (26, 0);
+        int start=0,end=0;
+        while (end < s2.size()) {
+            check[s2[end]-'a']++;
+            if (end - start + 1 == s1.size()) 
             {
-                check[s2[j]-'a']++;
-                // cout<<s2[j]-'a'<<" ";
+                if (hashh == check) return true;
             }
-            // cout<<endl;
-            if(check==hashh) return true;
-
-            start++;
+            if (end - start + 1 < s1.size()) 
+            {
+                end++;
+            }
+            else {
+                check[s2[start]-'a']--;
+                start++;
+                end++;
+            }
         }
+
+        // int k=s1.size()-1;
+        // for(int i=k;i<s2.size();i++)
+        // {
+            
+        //     for(int j=start;j<=i;j++)
+        //     {
+        //         check[s2[j]-'a']++;
+        //         // cout<<s2[j]-'a'<<" ";
+        //     }
+        //     // cout<<endl;
+        //     if(check==hashh) return true;
+
+        //     start++;
+        // }
         return false;
     }
 };
