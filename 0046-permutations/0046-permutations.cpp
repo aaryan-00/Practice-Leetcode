bee9d354fb7d646f524e138f866140eb02c3a564
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-07 01:13:00 +0530

    Time: 0 ms (100%), Space: 7.7 MB (45.4%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+  // TC=n!*n
+
     // vector<vector<int>>ans;
     // void getPerm(vector<int>& nums,vector<int> &curr)
     // {
@@ -26,30 +28,49 @@ public:
     //     return ans;
     // }
 
-    vector<vector<int>>ans;
-    vector<int> curr;
-    void getPerm(vector<int>& nums)
-    {
-        if(curr.size()==nums.size())
-        {
-            ans.push_back(curr);
-            return;
-        }
-        for(auto &i:nums)
-        {
-            if(find(curr.begin(), curr.end(), i) == curr.end())
-            {
-                curr.push_back(i);
-                getPerm(nums);
-                curr.pop_back();
-            }
-        }
+    // vector<vector<int>>ans;
+    // vector<int> curr;
+    // void getPerm(vector<int>& nums)
+    // {
+    //     if(curr.size()==nums.size())
+    //     {
+    //         ans.push_back(curr);
+    //         return;
+    //     }
+    //     for(auto &i:nums)
+    //     {
+    //         if(find(curr.begin(), curr.end(), i) == curr.end())
+    //         {
+    //             curr.push_back(i);
+    //             getPerm(nums);
+    //             curr.pop_back();
+    //         }
+    //     }
 
+    // }
+    // vector<vector<int>> permute(vector<int>& nums) {
+    //     ans.clear();
+    //     curr.clear();
+    //     getPerm(nums);
+    //     return ans;
+    // }
+
+//without extra space 
+    void recurPermute(int index, vector < int > & nums, vector < vector < int >> & ans) {
+      if (index == nums.size()) {
+        ans.push_back(nums);
+        return;
+      }
+      for (int i = index; i < nums.size(); i++) {
+        swap(nums[index], nums[i]);
+        recurPermute(index + 1, nums, ans);
+        swap(nums[index], nums[i]);
+      }
     }
-    vector<vector<int>> permute(vector<int>& nums) {
-        ans.clear();
-        curr.clear();
-        getPerm(nums);
-        return ans;
+  public:
+    vector < vector < int >> permute(vector < int > & nums) {
+      vector < vector < int >> ans;
+      recurPermute(0, nums, ans);
+      return ans;
     }
 };
