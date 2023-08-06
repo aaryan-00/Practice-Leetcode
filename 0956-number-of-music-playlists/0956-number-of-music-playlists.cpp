cafb7fcbcf61ec2b42375a396c38c84ff2d5df24
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-06 13:34:42 +0530

    Time: 4 ms (75.68%), Space: 8 MB (50%) - LeetHub

---
+++ 0956-number-of-music-playlists/0956-number-of-music-playlists.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int m=1e9+7;
+    long long getAns(int n,int goal,int k,vector<vector<int>> &dp)
+    {
+        if(n==0 && goal==0)
+        {
+            return 1;
+        }
+        if(n==0 || goal==0)
+        {
+            return 0;
+        }
+        if(dp[n][goal]!=-1) return dp[n][goal];
+        //newSong
+        long long pick=getAns(n-1,goal-1,k,dp)*n;
+        // ans%=m;
+        // old song
+        // if(n>k)
+        // {
+        long long notpick=getAns(n,goal-1,k,dp)*max(0,n-k);
+        // } 
+        return dp[n][goal]=(pick+notpick)%m;
+
+    }
+    int numMusicPlaylists(int n, int goal, int k) {
+        vector<vector<int>> dp(n + 1, vector<int>(goal + 1, -1));
+        return getAns(n,goal,k,dp);
+    }
+};
