cdc445c938f293b750afeb656037e263ade477e8
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-07 12:30:01 +0530

    Time: 3 ms (82.89%), Space: 9.5 MB (19.73%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int i=0;
+        for(;i<matrix.size();i++)
+        {
+            if(matrix[i][0]>target)
+            {
+                break;
+            }
+        }
+        // cout<<i<<endl;
+        if(i==0) return false;
+        i--;
+        int lo=0,hi=matrix[0].size()-1;
+        while(lo<=hi)
+        {
+            int mid=hi+lo>>1;
+            if(matrix[i][mid]==target) return true;
+            else if(matrix[i][mid]<target) lo=mid+1;
+            else hi=mid-1;
+        }
+        return false;
+    }
+
+};
