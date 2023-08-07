8499ebc814fd4a694996e9767c038e2ab5a317b7
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-07 12:40:20 +0530

    Time: 0 ms (100%), Space: 9.5 MB (19.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
+    // bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    //     int i=0;
+    //     for(;i<matrix.size();i++)
+    //     {
+    //         if(matrix[i][0]>target)
+    //         {
+    //             break;
+    //         }
+    //     }
+    //     // cout<<i<<endl;
+    //     if(i==0) return false;
+    //     i--;
+    //     int lo=0,hi=matrix[0].size()-1;
+    //     while(lo<=hi)
+    //     {
+    //         int mid=hi+lo>>1;
+    //         if(matrix[i][mid]==target) return true;
+    //         else if(matrix[i][mid]<target) lo=mid+1;
+    //         else hi=mid-1;
+    //     }
+    //     return false;
+    // }
+
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int i=0;
-        for(;i<matrix.size();i++)
+        int lo=0,hi=matrix.size()-1;
+        while(lo<=hi)
         {
-            if(matrix[i][0]>target)
-            {
-                break;
-            }
+            int mid= hi+lo >>1;
+            if(matrix[mid][0]<=target) lo=mid+1;
+            else hi=mid-1;
         }
-        // cout<<i<<endl;
-        if(i==0) return false;
-        i--;
-        int lo=0,hi=matrix[0].size()-1;
+        cout<<hi<<endl;
+        if(hi<0) return false;
+        int i=hi;
+        lo=0,hi=matrix[0].size()-1;
         while(lo<=hi)
         {
             int mid=hi+lo>>1;
@@ -22,5 +45,7 @@ public:
         }
         return false;
     }
+
+        
 
 };
