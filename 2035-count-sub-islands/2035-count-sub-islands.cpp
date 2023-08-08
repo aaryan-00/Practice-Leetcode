212996ccf0a985cfe3c77c5bff56cbb6763fb95c
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-08 22:42:28 +0530

    Time: 353 ms (72.55%), Space: 91.3 MB (55.62%) - LeetHub

---
+++ 2035-count-sub-islands/2035-count-sub-islands.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2,bool &check,int i,int j)
+    {
+        if(i<0||j<0||i==grid1.size() || j==grid1[0].size()||grid2[i][j]==0) return;
+        if(grid1[i][j]!=1) 
+        {
+            check=false;
+        }
+        grid2[i][j]=0;
+        dfs(grid1,grid2,check,i+1,j);
+        dfs(grid1,grid2,check,i-1,j);
+        dfs(grid1,grid2,check,i,j+1);
+        dfs(grid1,grid2,check,i,j-1);
+    }
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2)
+    {
+        int n=grid1.size();
+        int m=grid1[0].size();
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid2[i][j]==1)
+                {
+                    bool check=true;
+                    cout<<i<<" "<<j<<" ";
+                    dfs(grid1,grid2,check,i,j);
+                    cout<<check<<endl;
+                    ans+=check;
+                }
+                
+            }
+        }
+        return ans;
+
+    }
+};
