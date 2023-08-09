3c18b9e464bb2ae4f9bd750e29c858a6b84e893f
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-10 00:27:17 +0530

    Time: 260 ms (94.72%), Space: 108.2 MB (67.81%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -1,5 +1,45 @@
 class Solution {
 public:
+    // vector<int> paths={-1,0,1,0,-1};
+    // vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+    //     int n=isWater.size();
+    //     int m=isWater[0].size();
+    //     queue<pair<int,int>> q;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=0;j<m;j++)
+    //         {
+    //             if(isWater[i][j]==1) q.push({i,j});
+    //         }
+    //     }
+    //     while(!q.empty())
+    //     {
+    //         pair<int,int> curr=q.front();
+    //         q.pop();
+    //         int i=curr.first;
+    //         int j=curr.second;
+    //         for(int curr=0;curr<4;curr++)
+    //         {
+    //             int nextI=i+paths[curr];
+    //             int nextJ=j+paths[curr+1];
+    //             if(nextI>=0 && nextI<n && nextJ>=0 && nextJ<m && isWater[nextI][nextJ]==0)
+    //             {
+    //                 isWater[nextI][nextJ]=isWater[i][j]+1;
+    //                 q.push({nextI,nextJ});
+    //             }
+    //         }
+    //     }
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=0;j<m;j++)
+    //         {
+    //             isWater[i][j]--;
+    //         }
+    //     }
+    //     return isWater;  
+    // }
+
+
     vector<int> paths={-1,0,1,0,-1};
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
         int n=isWater.size();
@@ -9,10 +49,18 @@ public:
         {
             for(int j=0;j<m;j++)
             {
-                if(isWater[i][j]==1) q.push({i,j});
+                if(isWater[i][j]==1) 
+                {
+                    q.push({i,j});
+                    isWater[i][j]=0;
+                }
+                else
+                {
+                    isWater[i][j]=-1;
+                }
+
             }
         }
-        // int level=0;
         while(!q.empty())
         {
             pair<int,int> curr=q.front();
@@ -23,7 +71,7 @@ public:
             {
                 int nextI=i+paths[curr];
                 int nextJ=j+paths[curr+1];
-                if(nextI>=0 && nextI<n && nextJ>=0 && nextJ<m && isWater[nextI][nextJ]==0)
+                if(nextI>=0 && nextI<n && nextJ>=0 && nextJ<m && isWater[nextI][nextJ]==-1)
                 {
                     isWater[nextI][nextJ]=isWater[i][j]+1;
                     q.push({nextI,nextJ});
@@ -31,14 +79,6 @@ public:
             }
 
         }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                isWater[i][j]--;
-            }
-        }
-        return isWater;
-        
+        return isWater;  
     }
 };
