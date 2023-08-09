1a47f6f3c8e207e2f391e39723cd80ca6fa5f1c0
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-10 00:22:40 +0530

    Time: 277 ms (91.03%), Space: 108.2 MB (67.81%) - LeetHub

---
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> paths={-1,0,1,0,-1};
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int n=isWater.size();
+        int m=isWater[0].size();
+        queue<pair<int,int>> q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(isWater[i][j]==1) q.push({i,j});
+            }
+        }
+        // int level=0;
+        while(!q.empty())
+        {
+            pair<int,int> curr=q.front();
+            q.pop();
+            int i=curr.first;
+            int j=curr.second;
+            for(int curr=0;curr<4;curr++)
+            {
+                int nextI=i+paths[curr];
+                int nextJ=j+paths[curr+1];
+                if(nextI>=0 && nextI<n && nextJ>=0 && nextJ<m && isWater[nextI][nextJ]==0)
+                {
+                    isWater[nextI][nextJ]=isWater[i][j]+1;
+                    q.push({nextI,nextJ});
+                }
+            }
+
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                isWater[i][j]--;
+            }
+        }
+        return isWater;
+        
+    }
+};
