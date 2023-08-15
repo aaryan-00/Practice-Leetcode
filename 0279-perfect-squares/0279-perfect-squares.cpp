5751c42c4174aeaef22665cb00dea18f263e2cda
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-15 17:25:54 +0530

    Time: 149 ms (79.71%), Space: 9.7 MB (30.94%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> dp;
+    int getAns(int n,int sq,int sum)
+    {
+        if(sum==n) return 0;
+        if(dp[sum]!=-1) return dp[sum];
+        int ans=INT_MAX;
+        for(int i=1;i<=sq;i++)
+        {
+            if(i*i<=n-sum)
+            {
+                ans=min(ans,1+getAns(n,sq,sum+i*i));
+            }
+            else 
+            {
+                break;
+            }
+        }
+        return dp[sum]=ans;
+    }
+    int numSquares(int n) {
+        int sq=sqrt(n);
+        dp.resize(n+1,-1);
+        return getAns(n,sq,0);
+    }
+};
