b595b17e3182572c1eaa3a818967a3b45145e1f8
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-17 13:37:32 +0530

    Time: 2 ms (55.55%), Space: 6 MB (67.03%) - LeetHub

--- 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+    int dp[51][51];
     int getAns(int n,int ind)
     {
         if(n==0) return 1;
+        if(dp[n][ind]!=-1) return dp[n][ind];
         int sum=0;
         for(int i=ind;i<=5;i++)
         {
             sum+=getAns(n-1,i);
         }
-        return sum;
+        return dp[n][ind]=sum;
     }
     int countVowelStrings(int n) {
-        if(n==1) return 5;
+        memset(dp,-1,sizeof(dp));
         return getAns(n,1);
     }
 };
