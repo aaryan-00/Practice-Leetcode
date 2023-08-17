325b00c4d3b055aeba05edeb26f5652c6579b3e9
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-17 18:38:28 +0530

    Time: 122 ms (64.49%), Space: 9 MB (74.11%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -15,11 +15,33 @@ public:
         }
         return dp[ind][cnt]=ans;
     }
+    // int numTeams(vector<int>& rating) {
+    //     memset(dp,-1,sizeof(dp));
+    //     int res = getAns(rating,0,0,-1);
+    //     reverse(rating.begin(),rating.end());
+    //     memset(dp,-1,sizeof(dp));
+    //     return res+=getAns(rating,0,0,-1);
+    // }
+
+
     int numTeams(vector<int>& rating) {
-        memset(dp,-1,sizeof(dp));
-        int res = getAns(rating,0,0,-1);
-        reverse(rating.begin(),rating.end());
-        memset(dp,-1,sizeof(dp));
-        return res+=getAns(rating,0,0,-1);
+        int ans=0;
+        for(int i=1;i<rating.size()-1;i++)
+        {
+            int leftSmall=0,leftLarge=0;
+            int rightSmall=0,rightLarge=0;
+            for(int j=0;j<i;j++)
+            {
+                if(rating[j]<rating[i])leftSmall++;
+                if(rating[j]>rating[i])leftLarge++;
+            }
+            for(int j=i+1;j<rating.size();j++)
+            {
+                if(rating[j]<rating[i])rightSmall++;
+                if(rating[j]>rating[i])rightLarge++;
+            }
+            ans+=leftSmall*rightLarge +leftLarge*rightSmall;
+        }
+        return ans;
     }
 };
