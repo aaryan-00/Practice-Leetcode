8530e0bd340a4d86372886931b366f8653b265b3
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-20 18:08:07 +0530

    Time: 160 ms (91.96%), Space: 14.4 MB (77.08%) - LeetHub

--- 1808-stone-game-vii/1808-stone-game-vii.cpp
+++ 1808-stone-game-vii/1808-stone-game-vii.cpp
@@ -2,22 +2,29 @@ class Solution {
 public:
     vector<int> preSum;
     int dp[1001][1001];
-    int getAns(vector<int> &stones, int low,int high,int alice)
-    {
+    // int getAns(vector<int> &stones, int low,int high,int alice)
+    // {
+    //     if(low==high) return 0;
+    //     if(dp[low][high]!=-1) return dp[low][high];
+    //         int diff=INT_MIN;
+    //         diff=max(diff,preSum[high+1]-preSum[low+1]-getAns(stones,low+1,high,!alice));
+    //         diff=max(diff,preSum[high]-preSum[low]-getAns(stones,low,high-1,!alice));
+    //         return dp[low][high]= diff;
+    // }
+    
+    int getAns(vector<int> &stones, int low,int high,int alice){
         if(low==high) return 0;
         if(dp[low][high]!=-1) return dp[low][high];
-        // if(alice)
-        // {
-            int diff=INT_MIN;
-            diff=max(diff,preSum[high+1]-preSum[low+1]-getAns(stones,low+1,high,!alice));
-            diff=max(diff,preSum[high]-preSum[low]-getAns(stones,low,high-1,!alice));
-            return dp[low][high]= diff;
-        // }
-        // int diff=INT_MAX;
-        //     diff=min(diff,preSum[high+1]-preSum[low+1]-getAns(stones,low+1,high,!alice));
-        //     diff=min(diff,preSum[high]-preSum[low]-getAns(stones,low,high-1,!alice));
-        // return diff;
-
+        
+        if(alice){//maximize Alice's gain
+            int left = preSum[high+1]-preSum[low+1] + getAns(stones, low + 1, high, !alice);
+            int right = preSum[high]-preSum[low] + getAns(stones, low, high - 1, !alice);
+            return dp[low][high] = max(left, right);
+        }
+         // minimize Bob's gain
+        int left = getAns(stones, low + 1, high, !alice) - (preSum[high+1]-preSum[low+1]);
+        int right = getAns(stones, low, high - 1, !alice) - (preSum[high]-preSum[low]);
+        return dp[low][high] = min(left, right);
     }
     int stoneGameVII(vector<int>& stones) {
         int n=stones.size();
@@ -26,11 +33,6 @@ public:
         {
             preSum[i+1]=preSum[i]+stones[i];
         }
-        // for(auto i:preSum)
-        // {
-        //     cout<<i<<" ";
-        // }
-        // cout<<endl;
         memset(dp,-1,sizeof(dp));
         return getAns(stones,0,n-1,1);
     }
