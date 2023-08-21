535d4273e5823158e8f414b110df14fd66b3040d
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-21 21:40:15 +0530

    Time: 8 ms (98.48%), Space: 13.4 MB (57.6%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
+    // bool repeatedSubstringPattern(string s) {
+        // int n=s.size();
+        // for(int i=n/2;i>=1;i--)
+        // {
+        //     if(n%i==0)
+        //     {
+        //         string curr=s.substr(i)+s.substr(0,i);
+        //         if(curr==s) return true;
+        //     }
+        // }
+        // return false;
+    // }
+
     bool repeatedSubstringPattern(string s) {
-        int n=s.size();
-        for(int i=n/2;i>=1;i--)
-        {
-            if(n%i==0)
-            {
-                string curr=s.substr(i)+s.substr(0,i);
-                if(curr==s) return true;
-            }
-        }
-        return false;
+        string doubled = s + s;
+        string sub = doubled.substr(1, doubled.size() - 2);
+        return sub.find(s) != string::npos;
     }
 };
