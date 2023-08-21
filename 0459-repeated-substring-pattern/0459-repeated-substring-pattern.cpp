cb32474843f65565a1089fee9f5759d141689741
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-21 21:35:04 +0530

    Time: 25 ms (40.2%), Space: 38.9 MB (30.28%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -4,9 +4,11 @@ public:
         int n=s.size();
         for(int i=n-1;i>=1;i--)
         {
-            string curr=s.substr(i)+s.substr(0,i);
-            // cout<<curr<<endl;
-            if(curr==s) return true;
+            if(n%i==0)
+            {
+                string curr=s.substr(i)+s.substr(0,i);
+                if(curr==s) return true;
+            }
         }
         return false;
     }
