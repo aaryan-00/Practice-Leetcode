3743a49a362cbce59c97b2719f7dce668975be13
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-22 23:28:35 +0530

    Time: 17 ms (43.95%), Space: 17.9 MB (7.15%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -4,42 +4,64 @@ public:
         stack<int> st;
         for(int i=0;i<asteroids.size();i++)
         {
-            if(!st.empty() && asteroids[i]<0)
-            {
-                while(!st.empty() && asteroids[i]<0)
-                {
-                    if(st.top()>0)
-                    {
-                        if(st.top()>abs(asteroids[i]))
-                        {
-                            break;
-                        }
-                        else if(st.top()<abs(asteroids[i]))
-                        {
-                            cout<<"cbjsb"<<endl;
-                            st.pop();
-                        }
-                        else
-                        {
-                            st.pop();
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        st.push(asteroids[i]);
-                        break;
-                    }
-                    if(st.empty())
-                    {
-                        st.push(asteroids[i]);
-                        break;
-                    }
-                }
+            // if(!st.empty() && asteroids[i]<0)
+            // {
+            //     while(!st.empty() && asteroids[i]<0)
+            //     {
+            //         if(st.top()>0)
+            //         {
+            //             if(st.top()>abs(asteroids[i]))
+            //             {
+            //                 break;
+            //             }
+            //             else if(st.top()<abs(asteroids[i]))
+            //             {
+            //                 st.pop();
+            //             }
+            //             else
+            //             {
+            //                 st.pop();
+            //                 break;
+            //             }
+            //         }
+            //         else
+            //         {
+            //             st.push(asteroids[i]);
+            //             break;
+            //         }
+            //         if(st.empty())
+            //         {
+            //             st.push(asteroids[i]);
+            //             break;
+            //         }
+            //     }
                 
+            // }
+            // else
+            //     st.push(asteroids[i]);
+            bool flag=1;
+            while(!st.empty() &&st.top()>0 && asteroids[i]<0)
+            {
+                if(st.top()>abs(asteroids[i]))
+                {
+                    flag=0;
+                    break;
+                }
+                else if(st.top()<abs(asteroids[i]))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.pop();
+                    flag=0;
+                    break;
+                }
             }
-            else
+            if(flag)
+            {   
                 st.push(asteroids[i]);
+            }
         }
         vector<int> ans;
         while(!st.empty())
