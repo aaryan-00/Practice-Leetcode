54af8c85ead56a5fa0fefe2ccdc32dac89151d7f
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-23 22:02:03 +0530

    Time: 110 ms (95.35%), Space: 66.2 MB (59.73%) - LeetHub

--- 2305-append-k-integers-with-minimal-sum/2305-append-k-integers-with-minimal-sum.cpp
+++ 2305-append-k-integers-with-minimal-sum/2305-append-k-integers-with-minimal-sum.cpp
@@ -2,18 +2,12 @@ class Solution {
 public:
     long long minimalKSum(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
-        for(auto i:nums)
-        {
-            cout<<i<<" ";
-        }
-        cout<<endl;
         long long prev=0;
         long long ans=0;
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]==prev) continue;
             int diff=nums[i]-prev-1;
-            cout<<diff<<endl;
             if(diff==0) 
             {
                 prev=nums[i];
@@ -32,18 +26,14 @@ public:
                 long long curr=nums[i]-1;
                 long long temp=curr*(curr+1)/2 - prev*(prev+1)/2;
                 k-=diff;
-                // cout<<temp<<endl;
                 ans+=temp;
                 prev=nums[i];
             }
         }
-        // cout<<ans<<endl;
         if(k!=0)
         {
             long long curr=prev+k;
-            // cout<<curr<<endl;
             long long temp=curr*(curr+1)/2 - prev*(prev+1)/2;
-            // cout<<temp<<endl;
             ans+=temp;
         }
 
