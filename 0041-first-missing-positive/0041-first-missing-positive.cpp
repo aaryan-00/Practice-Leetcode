8b56806185be33cabc69a0a8a9058c2504291820
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-24 14:54:39 +0530

    Time: 60 ms (65.55%), Space: 41.3 MB (27.47%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,18 +1,47 @@
 class Solution {
 public:
+    // int firstMissingPositive(vector<int>& nums) {
+    //     unordered_set<int> s;
+    //     for(auto i:nums)
+    //     {
+    //         if(i<=0) continue;
+    //         s.insert(i);
+    //     }
+    //     int maxi=*max_element(nums.begin(),nums.end());
+    //     if(maxi<=0) return 1;
+    //     for(int i=1;i<=maxi;i++)
+    //     {
+    //         if(s.count(i)==0) return i;
+    //     }
+    //     return maxi+1;
+    // }
     int firstMissingPositive(vector<int>& nums) {
-        unordered_set<int> s;
-        for(auto i:nums)
+        sort(nums.begin(),nums.end());
+        int iter=0;
+        while(iter<nums.size() && nums[iter]<=0)
         {
-            if(i<=0) continue;
-            s.insert(i);
+            iter++;
         }
-        int maxi=*max_element(nums.begin(),nums.end());
-        if(maxi<=0) return 1;
-        for(int i=1;i<=maxi;i++)
+        if(iter==nums.size()) return 1;
+        int curr=1,prev=0;
+        while(iter<nums.size())
         {
-            if(s.count(i)==0) return i;
+            if(prev==nums[iter])
+            {
+                iter++;
+            }
+            else
+            {
+                if(curr!=nums[iter])
+                {
+                    return curr;
+                }
+                prev=nums[iter];
+                iter++;
+                curr++;  
+            }
+ 
         }
-        return maxi+1;
+        return curr;
     }
 };
