e95c981f88bb51e7955b4cee517e84150f73f901
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-27 21:55:23 +0530

    Time: 313 ms (14.53%), Space: 68.3 MB (25.61%) - LeetHub

--- 2591-frog-jump-ii/2591-frog-jump-ii.cpp
+++ 2591-frog-jump-ii/2591-frog-jump-ii.cpp
@@ -1,46 +1,68 @@
 class Solution {
 public:
-    // bool check(vector<int> &stones,int mid)
-    // {
-    //     vector<bool> vis(stones.size(),false);
-    //     int curr=0,prev=0;
-    //     for(int i=0;i<stones.size()-1;i++)
-    //     {
-    //         curr+=stones[i+1]-stones[i];
-    //         if(curr>mid)
-    //         {
-    //             // if()
-    //             vis[i]=true;
-    //             curr=stones[i+1]-stones[i];
-    //         }
-    //     }
-    // }
-    // int maxJump(vector<int>& stones) {
-    //     int n=stones.size();
-    //     int low=1,high=stones[n-1];
-    //     int ans=high;
-    //     while(low<high)
-    //     {
-    //         int mid=(low+high)>>1;
-    //         if(check(stones,mid))
-    //         {
-    //             ans=min(ans,mid);
-    //             high=mid-1;
-    //         }
-    //         else
-    //         {
-    //             low=mid+1;
-    //         }
-    //     }
-    //     return ans;
-    // }
-
-    int maxJump(vector<int>& stones) {
-        int res, n = stones.size();
-        res = stones[1] - stones[0];
-        for (int i = 2; i < n; i++) {
-            res = max(res, stones[i] - stones[i - 2]);
+    bool check(vector<int> &stones,int mid)
+    {
+        int n = stones.size();
+        vector<bool> vis(n, false);
+        
+        int i = 1, cur = 0, prev = 0;
+        while (i < n) {
+            if ((stones[i] - stones[cur]) > mid) {
+                if (prev != cur && !vis[prev]) {
+                    cur = prev;
+                    vis[prev] = true;
+                } else {
+                    return false;
+                }
+            } else {
+                prev = i++;
+            }
         }
-        return res;
+        
+        cur = 0; prev = 0; i = 1;
+        while (i < n) {
+            if (vis[i]) {
+                ++i; continue;
+            }
+            if ((stones[i] - stones[cur]) > mid) {
+                if (prev != cur) {
+                    cur = prev;
+                } else {
+                    return false;
+                }
+            } else {
+                prev = i++;
+            }
+        }
+        
+        return true;
     }
+    int maxJump(vector<int>& stones) {
+        int n=stones.size();
+        int low=0,high=1e9;
+        int ans=stones[n-1]-stones[0];
+        while(low<=high)
+        {
+            int mid=(low+high)>>1;
+            if(check(stones,mid))
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+
+    // int maxJump(vector<int>& stones) {
+    //     int res, n = stones.size();
+    //     res = stones[1] - stones[0];
+    //     for (int i = 2; i < n; i++) {
+    //         res = max(res, stones[i] - stones[i - 2]);
+    //     }
+    //     return res;
+    // }
 };
