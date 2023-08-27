5fb67bed478b17cf0f077a875bbd562de9df2ec1
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-27 21:29:34 +0530

    Time: 110 ms (79.24%), Space: 65.4 MB (56.89%) - LeetHub

---
+++ 2591-frog-jump-ii/2591-frog-jump-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    // bool check(vector<int> &stones,int mid)
+    // {
+    //     vector<bool> vis(stones.size(),false);
+    //     int curr=0,prev=0;
+    //     for(int i=0;i<stones.size()-1;i++)
+    //     {
+    //         curr+=stones[i+1]-stones[i];
+    //         if(curr>mid)
+    //         {
+    //             // if()
+    //             vis[i]=true;
+    //             curr=stones[i+1]-stones[i];
+    //         }
+    //     }
+    // }
+    // int maxJump(vector<int>& stones) {
+    //     int n=stones.size();
+    //     int low=1,high=stones[n-1];
+    //     int ans=high;
+    //     while(low<high)
+    //     {
+    //         int mid=(low+high)>>1;
+    //         if(check(stones,mid))
+    //         {
+    //             ans=min(ans,mid);
+    //             high=mid-1;
+    //         }
+    //         else
+    //         {
+    //             low=mid+1;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    int maxJump(vector<int>& stones) {
+        int res, n = stones.size();
+        res = max(stones[1] - stones[0], 0);
+        for (int i = 2; i < n; i++) {
+            res = max(res, stones[i] - stones[i - 2]);
+        }
+        return res;
+    }
+};
