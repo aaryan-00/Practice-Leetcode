9d91d1371039ddd0506281af01898e3a6578a18a
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-08-27 23:53:24 +0530

    Time: 249 ms (33.92%), Space: 17.8 MB (15.62%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -3,12 +3,12 @@ public:
     int dp[10001];
     int getAns(vector<int> &nums,int ind)
     {
-        if(ind>=nums.size()-1) return 0;
+        if(ind==nums.size()-1) return 0;
         if(dp[ind]!=-1) return dp[ind];
         int ans=1e6;
-        for(int i=1;i<=nums[ind];i++)
+        for(int i=ind+1;i<=min((int)nums.size()-1,ind+nums[ind]);i++)
         {
-            int curr=1+getAns(nums,i+ind);
+            int curr=1+getAns(nums,i);
             ans=min(ans,curr);
         }
         return dp[ind]=ans;
