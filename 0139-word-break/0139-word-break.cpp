447ad0f232cc26f8a84c270aa7891115e6e519a5
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-02 14:55:46 +0530

    Time: 7 ms (68.24%), Space: 8.4 MB (77.36%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    // unordered_set<string> ss;
+    // // int dp[301];
+    // vector<int> dp;
+    // bool getAns(string &s,int ind)
+    // {
+    //     if(ind==s.size()) return true;
+    //     if(dp[ind]!=-1) return dp[ind];
+    //     bool ans=false;
+    //     for(int i=ind+1;i<=s.size();i++)
+    //     {
+    //         string temp=s.substr(ind,i-ind);
+    //         if(ss.count(temp))
+    //         {
+    //             ans=ans|getAns(s,i);
+    //         }
+    //     }
+    //     return dp[ind]=ans;
+    // }
+    // bool wordBreak(string s, vector<string>& wordDict) {
+    //     for(auto &i:wordDict)
+    //     {
+    //         ss.insert(i);
+    //     }
+    //     // memset(dp,-1,sizeof(dp));
+    //     dp.resize(s.size(),-1);
+    //     return getAns(s,0);
+    // }
+
+
+
     unordered_set<string> ss;
     // int dp[301];
     vector<int> dp;
@@ -8,12 +38,13 @@ public:
         if(ind==s.size()) return true;
         if(dp[ind]!=-1) return dp[ind];
         bool ans=false;
-        for(int i=ind+1;i<=s.size();i++)
+        string temp="";
+        for(int i=ind;i<s.size();i++)
         {
-            string temp=s.substr(ind,i-ind);
+            temp+=s[i];
             if(ss.count(temp))
             {
-                ans=ans|getAns(s,i);
+                ans=ans|getAns(s,i+1);
             }
         }
         return dp[ind]=ans;
