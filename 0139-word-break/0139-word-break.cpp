88131e56884fb920599d05901084129d3265dd97
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-02 13:03:35 +0530

    Time: 23 ms (25.79%), Space: 13.5 MB (35.88%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    set<string> ss;
+    int dp[301];
+    bool getAns(string &s,int ind)
+    {
+        if(ind==s.size()) return true;
+        if(dp[ind]!=-1) return dp[ind];
+        bool ans=false;
+        for(int i=ind+1;i<=s.size();i++)
+        {
+            string temp=s.substr(ind,i-ind);
+            if(ss.count(temp))
+            {
+                ans=ans|getAns(s,i);
+            }
+        }
+        return dp[ind]=ans;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        for(auto &i:wordDict)
+        {
+            ss.insert(i);
+        }
+        memset(dp,-1,sizeof(dp));
+        return getAns(s,0);
+    }
+};
