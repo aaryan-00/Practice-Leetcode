928da304533164d19e9c1b5b13895bb74860c62a
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-02 15:41:21 +0530

    Time: 129 ms (98.61%), Space: 40.2 MB (98.21%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,20 +1,57 @@
 class Solution {
 public:
+    // unordered_set<string> ss;
+    // bool getAns(string &s,int ind,vector<int> &dp)
+    // {
+    //     if(ind==s.size()) return true;
+    //     if(dp[ind]!=-1) return dp[ind];
+    //     string temp="";
+    //     for(int i=ind;i<s.size();i++)
+    //     {
+    //         temp+=s[i];
+    //         if(ss.count(temp) && temp.size()<s.size())
+    //         {
+    //             if(getAns(s,i+1,dp))
+    //             {
+    //                 return dp[ind]=true;
+    //             }
+    //         }
+    //     }
+    //     return dp[ind]=false;
+    // }
+    // vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+    //     for(auto &i:words)
+    //     {
+    //         ss.insert(i);
+    //     }
+
+    //     vector<string> ans;
+    //     for(auto &i:words)
+    //     {
+    //         vector<int> dp(i.size(), -1);
+    //         if(getAns(i,0,dp))
+    //         {
+                
+    //             ans.push_back(i);
+    //         }
+    //     }
+    //     return ans;
+    // }
+
     unordered_set<string> ss;
-    bool getAns(string &s,int ind,vector<int> &dp)
+    vector<int> dp;
+    bool getAns(string &s,int ind)
     {
         if(ind==s.size()) return true;
         if(dp[ind]!=-1) return dp[ind];
+        bool ans=false;
         string temp="";
         for(int i=ind;i<s.size();i++)
         {
             temp+=s[i];
-            if(ss.count(temp) && temp.size()<s.size())
+            if(ss.count(temp) && temp!=s)
             {
-                if(getAns(s,i+1,dp))
-                {
-                    return dp[ind]=true;
-                }
+                if(getAns(s,i+1)) return dp[ind]=true;
             }
         }
         return dp[ind]=false;
@@ -24,17 +61,17 @@ public:
         {
             ss.insert(i);
         }
-
         vector<string> ans;
         for(auto &i:words)
         {
-            vector<int> dp(i.size(), -1);
-            if(getAns(i,0,dp))
+            dp.clear();
+            dp.resize(i.size(),-1);
+            if(getAns(i,0))
             {
-                
                 ans.push_back(i);
             }
         }
         return ans;
     }
+
 };
