efe8d077a269a6aff79cba0135781e7c351ea75c
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-02 15:35:47 +0530

    Time: 147 ms (95.6%), Space: 48.2 MB (75.28%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    unordered_set<string> ss;
+    bool getAns(string &s,int ind,vector<int> &dp)
+    {
+        if(ind==s.size()) return true;
+        if(dp[ind]!=-1) return dp[ind];
+        string temp="";
+        for(int i=ind;i<s.size();i++)
+        {
+            temp+=s[i];
+            if(ss.count(temp) && temp.size()<s.size())
+            {
+                if(getAns(s,i+1,dp))
+                {
+                    return dp[ind]=true;
+                }
+            }
+        }
+        return dp[ind]=false;
+    }
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        for(auto &i:words)
+        {
+            ss.insert(i);
+        }
+
+        vector<string> ans;
+        for(auto &i:words)
+        {
+            vector<int> dp(i.size(), -1);
+            if(getAns(i,0,dp))
+            {
+                
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
