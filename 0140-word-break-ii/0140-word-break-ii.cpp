2ce8b5ee1ff62f603c4eb8747e201e693fb65194
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-02 14:57:26 +0530

    Time: 0 ms (100%), Space: 7.3 MB (73.1%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,5 +1,48 @@
 class Solution {
 public:
+    // unordered_set<string> ss;
+    // vector<string> ans;
+    // vector<string> curr;
+    // void getAns(string &s,int ind)
+    // {
+    //     if(ind==s.size()) 
+    //     {
+            
+    //         if(curr.size()>0)
+    //         {
+    //             string alpha="";
+    //             for(auto &i:curr)
+    //             {
+    //                 alpha+=i;
+    //                 alpha+=" ";
+    //             }
+    //             alpha.pop_back();
+    //             ans.push_back(alpha);
+    //         }
+    //         return;
+    //     }
+    //     for(int i=ind+1;i<=s.size();i++)
+    //     {
+    //         string temp=s.substr(ind,i-ind);
+    //         if(ss.count(temp))
+    //         {
+    //             curr.push_back(temp);
+    //             getAns(s,i);
+    //             curr.pop_back();
+    //         }
+    //     }
+    // }
+    // vector<string> wordBreak(string s, vector<string>& wordDict) {
+    //     for(auto &i:wordDict)
+    //     {
+    //         ss.insert(i);
+    //     }
+    //     ans.clear();
+    //     getAns(s,0);
+    //     return ans;
+    // }
+
+
     unordered_set<string> ss;
     vector<string> ans;
     vector<string> curr;
@@ -7,7 +50,6 @@ public:
     {
         if(ind==s.size()) 
         {
-            
             if(curr.size()>0)
             {
                 string alpha="";
@@ -21,13 +63,14 @@ public:
             }
             return;
         }
-        for(int i=ind+1;i<=s.size();i++)
+        string temp="";
+        for(int i=ind;i<s.size();i++)
         {
-            string temp=s.substr(ind,i-ind);
+            temp+=s[i];
             if(ss.count(temp))
             {
                 curr.push_back(temp);
-                getAns(s,i);
+                getAns(s,i+1);
                 curr.pop_back();
             }
         }
