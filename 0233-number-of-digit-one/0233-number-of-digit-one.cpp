3f5854c5ac21ae5cfd1ce9dfe8f32dcb82c47b3d
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-05 23:33:08 +0530

    Time: 2 ms (52.91%), Space: 6.1 MB (38.57%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.cpp
+++ 0233-number-of-digit-one/0233-number-of-digit-one.cpp
@@ -2,7 +2,7 @@
 typedef vector<ll> vi;
 class Solution {
 public:
-    ll dp[11][2][11];
+    ll dp[10][2][10];
     ll getAns(vi &num, ll pos, bool tight,int cnt)
     {
         if (pos == num.size())
@@ -36,4 +36,40 @@ public:
         memset(dp, -1, sizeof(dp));
         return getAns(curr,0,1,0);
     }
+
+
+    // ll dp[11][2];
+    // ll getAns(vi &num, ll pos, bool tight)
+    // {
+    //     if (pos == num.size())
+    //     {
+    //         return 0;
+    //     }
+    //     if (dp[pos][tight] != -1)
+    //     {
+    //         return dp[pos][tight];
+    //     }
+    //     ll ans = 0;
+    //     ll ub = 9;
+    //     if (tight == 1)
+    //     {
+    //         ub = num[pos];
+    //     }
+    //     for (ll dig = 0; dig <= ub; dig++)
+    //     {
+    //         ans += (dig==1)+ getAns(num, pos + 1, tight & (dig == ub));
+    //     }
+    //     return dp[pos][tight] = ans;
+    // }
+    // int countDigitOne(int n) {
+    //     vi curr;
+    //     while (n > 0)
+    //     {
+    //         curr.push_back(n%10);
+    //         n /= 10;
+    //     }
+    //     reverse(curr.begin(),curr.end());
+    //     memset(dp, -1, sizeof(dp));
+    //     return getAns(curr,0,1);
+    // }
 };
