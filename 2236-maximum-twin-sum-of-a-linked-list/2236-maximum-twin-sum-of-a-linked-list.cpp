1f17cec2714504e6c2e2dab6472d6a10f9060445
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-06 15:07:27 +0530

    Time: 181 ms (87.71%), Space: 116.5 MB (85.53%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
@@ -10,23 +10,50 @@
  */
 class Solution {
 public:
+    // int pairSum(ListNode* head) {
+    //     stack<ListNode*> st;
+    //     //finding mid
+    //     ListNode *slow = head, *fast = head;
+    //     while(fast != NULL && fast->next != NULL)
+    //     {
+    //         st.push(slow);
+    //         fast = fast->next->next;
+    //         slow = slow->next;
+    //     }
+    //     // cout<<st.size()<<endl;
+    //     int ans=0;
+    //     while(slow!=nullptr)
+    //     {
+    //         ans=max(ans,st.top()->val+slow->val);
+    //         st.pop();
+    //         slow=slow->next;
+    //     }
+    //     return ans;
+    // }
+
     int pairSum(ListNode* head) {
-        stack<ListNode*> st;
         //finding mid
         ListNode *slow = head, *fast = head;
         while(fast != NULL && fast->next != NULL)
         {
-            st.push(slow);
             fast = fast->next->next;
             slow = slow->next;
         }
-        // cout<<st.size()<<endl;
         int ans=0;
-        while(slow!=nullptr)
+        ListNode *prev = NULL;
+        ListNode *next = NULL;
+        while (slow != NULL)
         {
-            ans=max(ans,st.top()->val+slow->val);
-            st.pop();
-            slow=slow->next;
+            next = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = next;
+        }
+        while(prev)
+        {
+            ans=max(ans,head->val+prev->val);
+            prev=prev->next;
+            head=head->next;
         }
         return ans;
     }
