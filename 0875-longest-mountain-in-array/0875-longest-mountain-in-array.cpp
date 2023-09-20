fbc5155ccf349e398b9d2b9cc79541ddaf1a2f0d
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-20 14:44:14 +0530

    Time: 19 ms (38.65%), Space: 18.9 MB (37.28%) - LeetHub

--- 0875-longest-mountain-in-array/0875-longest-mountain-in-array.cpp
+++ 0875-longest-mountain-in-array/0875-longest-mountain-in-array.cpp
@@ -1,62 +1,60 @@
 class Solution {
 public:
-int ans=0;
-    void getmaxi(vector<int> &arr,int left,int right)
-    {
-        left--,right++;
-        int lcnt=0,rcnt=0;
-        while(left>=0)
-        {
-            if(arr[left]<arr[left+1])
-            {
-                left--;
-                lcnt++;
-            }
-            else break;
-        }
-        while(right<arr.size())
-        {
-            if(arr[right]<arr[right-1])
-            {
-                right++;
-                rcnt++;
-            }
-            else break;
-        }
-        if(lcnt && rcnt)
-        {
+// int ans=0;
+//     void getmaxi(vector<int> &arr,int left,int right)
+//     {
+//         left--,right++;
+//         int lcnt=0,rcnt=0;
+//         while(left>=0)
+//         {
+//             if(arr[left]<arr[left+1])
+//             {
+//                 left--;
+//                 lcnt++;
+//             }
+//             else break;
+//         }
+//         while(right<arr.size())
+//         {
+//             if(arr[right]<arr[right-1])
+//             {
+//                 right++;
+//                 rcnt++;
+//             }
+//             else break;
+//         }
+//         if(lcnt && rcnt)
+//         {
             
-            ans=max(ans,lcnt+rcnt+1);
-        }
-    }
-    // void getmaxieven(vector<int> &arr,int left,int right)
-    // {
-    //     int one=left ,two=right;
-    //     left--,right++;
-    //     int cnt=2;
-    //     while(left>=0 && right<arr.size())
-    //     {
-    //         if(arr[left]<arr[left+1] && arr[right]<arr[right-1])
-    //         {
-    //             left--,right++;
-    //             cnt+=2;
-    //         }
-    //         else break;
-    //     }
-    //     if(cnt>2)
-    //     {
-    //         ans=max(ans,cnt);
-    //     }
-    // }
+//             ans=max(ans,lcnt+rcnt+1);
+//         }
+//     }
+
     int longestMountain(vector<int>& arr) {
         int n=arr.size();
-        // cout<<n<<endl;
-        for(int i=0;i<n;i++)
+        // for(int i=0;i<n;i++)
+        // {
+        //     getmaxi(arr,i,i);
+        // }
+        // return ans;
+
+        int s=0,e;
+        int maxi=0;
+        while(s<n)
         {
-            getmaxi(arr,i,i);
-            // if(arr[i]!=arr[i+1])
-            //     getmaxi(arr,i,i+1);
+            e=s;
+            if(e+1<n && arr[e+1]>arr[e])
+            {
+                while(e+1<n && arr[e+1]>arr[e]) e++;
+                //arr[e] is peak;
+                if(e+1<n && arr[e+1]<arr[e])
+                {
+                    while(e+1<n && arr[e+1]<arr[e]) e++;
+                    maxi=max(maxi,e-s+1);
+                }
+            }
+            s=max(e,s+1);
         }
-        return ans;
+        return maxi;
     }
 };
