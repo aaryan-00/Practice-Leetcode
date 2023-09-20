b4460266f73978823f9b15e62eec3b10861a0ccb
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-20 20:01:08 +0530

    Time: 369 ms (15.24%), Space: 177.5 MB (5.04%) - LeetHub

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int n=nums.size();
+        if(nums[0]>x and nums[n-1]>x) return -1;
+        vector<int> presum(n+1,0);
+        unordered_map<int,int> mp;
+        mp[0]=0;
+        for(int i=1;i<=n;i++)
+        {
+            presum[i]=presum[i-1]+nums[i-1];
+            mp[presum[i]]=i;
+        }
+        if(presum[n]<x) return -1;
+        vector<int> suff(n+1,0);
+        for(int i=n-1;i>=0;i--)
+        {
+            suff[i]=suff[i+1]+nums[i];
+        }
+        reverse(suff.begin(),suff.end());
+        int ans=INT_MAX;
+        for(int i=0;i<=n;i++)
+        {
+            if(suff[i]<=x && mp.find(x-suff[i])!=mp.end())
+            {
+                // cout<<i<<" "<<mp[x-suff[i]]<<endl; 
+                ans=min(ans,i+mp[x-suff[i]]);
+            }
+        }
+        if(ans==INT_MAX) return -1;
+        return ans;    
+    }
+};
