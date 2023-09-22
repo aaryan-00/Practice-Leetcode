0a6e4a6105ced825cf7e6851b5a8c91b9acf0265
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-22 18:26:52 +0530

    Time: 3 ms (76.95%), Space: 7.7 MB (15.05%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,39 +1,67 @@
+// class Solution {
+// public:
+//     int longestValidParentheses(string s) {
+//         int n =s.size();
+//         stack<int> st;
+//         int ans=0;
+//         for(int i=0;i<s.size();i++)
+//         {
+//             if(st.empty())
+//             {
+//                 st.push(i);
+//             }
+//             else if(s[i]=='(')
+//             {
+//                 st.push(i);
+//             }
+//             else
+//             {
+//                 if(s[st.top()]=='(') st.pop();
+//                 else st.push(i);
+//             }
+//         }
+//         // cout<<st.size()<<endl;
+//         if(st.empty()) return n;
+//         int end=n;
+//         while(!st.empty())
+//         {
+//             int start=st.top();
+//             st.pop();
+//             ans=max(ans,end-start-1);
+//             end=start;
+//         }
+//         ans=max(ans,end);
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int n =s.size();
-        stack<int> st;
-        int ans=0;
+        stack<int>st;
+        st.push(-1);
+        int maxl=0;
         for(int i=0;i<s.size();i++)
         {
-            if(st.empty())
-            {
-                st.push(i);
-            }
-            else if(s[i]=='(')
+            if(s[i]=='(')
             {
                 st.push(i);
             }
             else
             {
-                if(s[st.top()]=='(') 
+                st.pop();
+                if(st.empty())
                 {
-                    st.pop();
+                    st.push(i);
                 }
-                else st.push(i);
+                else
+                {
+                    maxl=max(maxl,i-st.top());
+                }
             }
         }
-        // cout<<st.size()<<endl;
-        if(st.empty()) return n;
-        int end=n;
-        while(!st.empty())
-        {
-            int start=st.top();
-            st.pop();
-            ans=max(ans,end-start-1);
-            end=start;
-        }
-        ans=max(ans,end);
-        return ans;
+        return maxl;
+        
     }
 };
