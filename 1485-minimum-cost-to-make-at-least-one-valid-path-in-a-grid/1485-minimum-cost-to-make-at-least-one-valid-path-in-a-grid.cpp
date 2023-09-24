b01b3a880a17c80d51714b7cc22c6957b866c094
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-24 19:00:09 +0530

    Time: 74 ms (47.31%), Space: 13.3 MB (94.38%) - LeetHub

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -5,13 +5,13 @@ public:
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
         pq.push({0,{0,0}});
         bool st=false;
-        vector<int> dx={0,1,0,-1};
-        vector<int> dy={1,0,-1,0};
-        unordered_map<int,pair<int,int>> mp;
-        mp[1]={0,1};
-        mp[2]={0,-1};
-        mp[3]={1,0};
-        mp[4]={-1,0};
+        vector<int> dx={0,0,1,-1};
+        vector<int> dy={1,-1,0,0};
+        // unordered_map<int,pair<int,int>> mp;
+        // mp[1]={0,1};
+        // mp[2]={0,-1};
+        // mp[3]={1,0};
+        // mp[4]={-1,0};
         vector<vector<bool>> vis(n,vector<bool>(m,false));
         while(!pq.empty())
         {
@@ -30,8 +30,16 @@ public:
                 if(newi==0 && newj==0) continue;
                 if(newi>=0 && newj>=0 && newi<n && newj<m && vis[newi][newj]==false)
                 {
-                    pair<int,int> dir=mp[grid[i][j]];
-                    if(dir.first==dx[it] && dir.second==dy[it])
+                    // pair<int,int> dir=mp[grid[i][j]];
+                    // if(dir.first==dx[it] && dir.second==dy[it])
+                    // {
+                    //     pq.push({cnt,{newi,newj}});
+                    // }
+                    // else
+                    // {
+                    //     pq.push({cnt+1,{newi,newj}});
+                    // }
+                    if(grid[i][j]==it+1)
                     {
                         pq.push({cnt,{newi,newj}});
                     }
