f55488fca8830cbfdad4172e81bbba0b94822e70
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-25 19:07:48 +0530

    Time: 2 ms (70.52%), Space: 6.9 MB (25.22%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // int i=0;
+        // while(i<s.size())
+        // {
+        //     if(s[i]!=t[i]) return t[i];
+        //     i++;
+        // }
+        // return t[i];
+        char ans=0;
         int i=0;
-        while(i<s.size())
+        while(i<t.size())
         {
-            if(s[i]!=t[i]) return t[i];
+            ans+=t[i];
+            if(i<t.size()-1)ans-=s[i];
             i++;
         }
-        return t[i];
+        return ans;
     }
 };
