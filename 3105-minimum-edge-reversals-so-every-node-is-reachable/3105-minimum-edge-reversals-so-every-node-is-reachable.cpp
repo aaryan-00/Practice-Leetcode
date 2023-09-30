bf4c5f55885e5b71424f12d7c698c7dc227f96ac
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-30 10:49:09 +0530

    Time: 394 ms (91.57%), Space: 190.4 MB (89.03%) - LeetHub

--- 3105-minimum-edge-reversals-so-every-node-is-reachable/3105-minimum-edge-reversals-so-every-node-is-reachable.cpp
+++ 3105-minimum-edge-reversals-so-every-node-is-reachable/3105-minimum-edge-reversals-so-every-node-is-reachable.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    void dfs(vector<int> &vis, vector<pair<int, int>> adj[], pair<int, int> node, int &ans, vector<int> &dp, int &fl)
+    void dfs(vector<int> &vis, vector<pair<int, int>> adj[], int node, int &ans, vector<int> &dp, int &fl)
     {
         // if(vis[node.first]==1) return ;
-        vis[node.first] = 1;
+        vis[node] = 1;
 
-        for (auto it : adj[node.first])
+        for (auto it : adj[node])
         {
             if (vis[it.first] == 0)
             {
@@ -16,10 +16,10 @@ public:
                         it.second = -1;
                     else
                         it.second = 1;
-                    dp[it.first] = dp[node.first] + it.second;
+                    dp[it.first] = dp[node] + it.second;
                     // cout << dp[it.first] << " " << it.first << " " << node.first << endl;
                 }
-                dfs(vis, adj, it, ans, dp, fl);
+                dfs(vis, adj, it.first, ans, dp, fl);
             }
         }
     }
@@ -30,31 +30,31 @@ public:
             adj[u].push_back({v,0});
             adj[v].push_back({u,1});
         }
-        for(int i=0;i<n;i++)
-        {
-            cout<<i<<":";
-            for(auto it:adj[i])
-            {
-                cout<<it.first<<","<<it.second<<" ";
-            }
-            cout<<endl;
-        }
+        // for(int i=0;i<n;i++)
+        // {
+        //     cout<<i<<":";
+        //     for(auto it:adj[i])
+        //     {
+        //         cout<<it.first<<","<<it.second<<" ";
+        //     }
+        //     cout<<endl;
+        // }
 
         vector<int>dp(n,0);
 
         int ans=0;
-        vector<int> vis(n+1,0);
+        vector<int> vis(n,0);
 
         int fl=0;
-        dfs(vis,adj,{0,adj[0][0].second},ans,dp,fl);
+        dfs(vis,adj,0,ans,dp,fl);
         dp[0]=ans;
         // cout<<ans<<"ans";
 
         fl=1;
         fill(vis.begin(),vis.end(),0) ;
         // cout<<adj[0][0].second ;
-        
-        dfs(vis,adj,{0,adj[0][0].second},ans,dp,fl);
+
+        dfs(vis,adj,0,ans,dp,fl);
         return dp;
 
     }
