60dd7e8dde479ebb43aaa7a57a854a5331c39566
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-09-30 10:58:42 +0530

    Time: 404 ms (87.08%), Space: 186.8 MB (92.35%) - LeetHub

--- 3105-minimum-edge-reversals-so-every-node-is-reachable/3105-minimum-edge-reversals-so-every-node-is-reachable.cpp
+++ 3105-minimum-edge-reversals-so-every-node-is-reachable/3105-minimum-edge-reversals-so-every-node-is-reachable.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    void dfs(vector<int> &vis, vector<pair<int, int>> adj[], int node, int &ans, vector<int> &dp, int &fl)
+    void dfs(vector<int> &vis, vector<pair<int, int>> adj[], int node, int &ans)
     {
-        // if(vis[node.first]==1) return ;
         vis[node] = 1;
 
         for (auto it : adj[node])
@@ -10,16 +9,26 @@ public:
             if (vis[it.first] == 0)
             {
                 ans = ans + it.second;
-                if (fl == 1)
-                {
-                    if (it.second == 1)
-                        it.second = -1;
-                    else
-                        it.second = 1;
-                    dp[it.first] = dp[node] + it.second;
+                dfs(vis, adj, it.first, ans);
+            }
+        }
+    }
+
+    void dfs2(vector<int> &vis, vector<pair<int, int>> adj[], int node, vector<int> &dp)
+    {
+        vis[node] = 1;
+
+        for (auto it : adj[node])
+        {
+            if (vis[it.first] == 0)
+            {
+                if (it.second == 1)
+                    it.second = -1;
+                else
+                    it.second = 1;
+                dp[it.first] = dp[node] + it.second;
                     // cout << dp[it.first] << " " << it.first << " " << node.first << endl;
-                }
-                dfs(vis, adj, it.first, ans, dp, fl);
+                dfs2(vis, adj, it.first, dp);
             }
         }
     }
@@ -46,15 +55,13 @@ public:
         vector<int> vis(n,0);
 
         int fl=0;
-        dfs(vis,adj,0,ans,dp,fl);
+        dfs(vis,adj,0,ans);
         dp[0]=ans;
-        // cout<<ans<<"ans";
 
         fl=1;
         fill(vis.begin(),vis.end(),0) ;
-        // cout<<adj[0][0].second ;
 
-        dfs(vis,adj,0,ans,dp,fl);
+        dfs2(vis,adj,0,dp);
         return dp;
 
     }
