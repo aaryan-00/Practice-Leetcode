640d79f848386fa2db43f72cacb101dcd7f47101
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-10-07 18:06:17 +0530

    Time: 111 ms (37.29%), Space: 76 MB (10.17%) - LeetHub

---
+++ 2084-maximum-number-of-weeks-for-which-you-can-work/2084-maximum-number-of-weeks-for-which-you-can-work.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long numberOfWeeks(vector<int>& milestones) {
+        long long maxi=-1,sum=0;
+        int n=milestones.size();
+        for(int i=0;i<n;i++)
+        {
+            sum+=milestones[i];
+            maxi=max(maxi,1LL*milestones[i]);
+        }
+        sum-=maxi;  
+        cout<<sum<<" "<<maxi<<endl; 
+        if(sum>=maxi) return sum+maxi;  
+        return sum*2+1;
+    }
+};
